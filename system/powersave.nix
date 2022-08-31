ba468fcef7ed3a4feab63f17bebd238c6c4b6b6d
Jacob Birkett <jacob@birkett.dev>
2022-08-30 17:17:25 -0700

    Add settings for handling low battery

--- system/powersave.nix
+++ system/powersave.nix
@@ -23,4 +23,22 @@
   systemd.sleep.extraConfig = ''
     HibernateDelaySec=${toString hibernate_delay}
   '';
+
+  services.upower = {
+    enable = true;
+    percentageLow = 15;
+    percentageCritical = 7;
+    percentageAction = 5;
+    criticalPowerAction = "Hibernate";
+  };
+
+  # add a shell command for all users to show the current
+  # status of the battery
+  environment.shellAliases = {
+    showbattery = ''
+      upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
+        | grep -E 'state|charge-cycles|time\ to|percentage' \
+        | sed 's/^ *//g'
+    '';
+  };
 }
