56223a9e314f0a339451e7bf870b41b3e85c9dac
Jacob Birkett <jacob@birkett.dev>
2022-09-01 02:15:52 -0700

    Disable writing kvantum and qt5ct config

--- users/jacob/theming.nix
+++ users/jacob/theming.nix
@@ -19,7 +19,7 @@
       };
       kde = {
         package = materia-kde-theme;
-        name = "MateriaDark";
+        # name = "MateriaDark";
       };
       fonts = {
         default = {
@@ -35,13 +35,16 @@
   in
 {
   # specify packages that are required for some stuff below
-  home.packages = with pkgs; [
+  home.packages = [
     # lxqt.lxqt-qtplugin
-    libsForQt5.qt5ct
-    libsForQt5.qtstyleplugin-kvantum
+    pkgs.libsForQt5.qt5ct
+    pkgs.libsForQt5.qtstyleplugin-kvantum
     theme.kde.package
     theme.fonts.monospace.package
   ];
+
+  # use qt5ct in tandem with kvantum
+  home.sessionVariables.QT_QPA_PLATFORMTHEME = "qt5ct";
 
   # specify packages to use for gtk theming
   gtk = {
@@ -73,12 +76,15 @@
     x11.enable = true;
   };
 
+  # should cover any other bases
+  home.sessionVariables.XCURSOR_SIZE = "${toString theme.cursor.size}";
+
   # set the kvantum theme, still needs qt5ct to be manually configured
   # expects pkgs.materia-kde-theme
-  xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
-    [General]
-    theme=${theme.kde.name}
-  '';
+  # xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
+  #   [General]
+  #   theme=${theme.kde.name}
+  # '';
 
   # # use lxqt-qtplugin because qt5ct uses a weird config format
   # # the font has more parameters, but this seems to work fine without
@@ -103,37 +109,31 @@
   # how the fonts work
   # they are dumped from a QFont object
   # <https://doc.qt.io/qt-5/qfont.html>
-  xdg.configFile."qt5ct/qt5ct.conf".text = ''
-    [Appearance]
-    color_scheme_path=${pkgs.libsForQt5.qt5ct}/share/qt5ct/colors/${if theme.dark then "darker" else "airy"}.conf
-    custom_palette=true
-    icon_theme=${theme.icons.name}
-    standard_dialogs=default
-    style=kvantum
+  # xdg.configFile."qt5ct/qt5ct.conf".text = ''
+  #   [Appearance]
+  #   color_scheme_path=${pkgs.libsForQt5.qt5ct}/share/qt5ct/colors/${if theme.dark then "darker" else "airy"}.conf
+  #   custom_palette=true
+  #   icon_theme=${theme.icons.name}
+  #   standard_dialogs=default
+  #   style=kvantum
 
-    [Fonts]
-    fixed=@Variant(\0\0\0@\0\0\0 \0\x44\0\x65\0j\0\x61\0V\0u\0 \0S\0\x61\0n\0s\0 \0M\0o\0n\0o@\"\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x91)
-    general=@Variant(\0\0\0@\0\0\0\f\0U\0\x62\0u\0n\0t\0u@\"\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
+  #   [Fonts]
+  #   fixed=@Variant(\0\0\0@\0\0\0 \0\x44\0\x65\0j\0\x61\0V\0u\0 \0S\0\x61\0n\0s\0 \0M\0o\0n\0o@\"\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x91)
+  #   general=@Variant(\0\0\0@\0\0\0\f\0U\0\x62\0u\0n\0t\0u@\"\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
 
-    [Interface]
-    activate_item_on_single_click=1
-    buttonbox_layout=0
-    cursor_flash_time=1000
-    dialog_buttons_have_icons=1
-    double_click_interval=400
-    gui_effects=@Invalid()
-    keyboard_scheme=2
-    menus_have_icons=true
-    show_shortcuts_in_context_menus=true
-    stylesheets=@Invalid()
-    toolbutton_style=4
-    underline_shortcut=1
-    wheel_scroll_lines=3
-  '';
-
-  home.sessionVariables = {
-    XCURSOR_SIZE = "${toString theme.cursor.size}";
-    # QT_QPA_PLATFORMTHEME = "lxqt";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-  };
+  #   [Interface]
+  #   activate_item_on_single_click=1
+  #   buttonbox_layout=0
+  #   cursor_flash_time=1000
+  #   dialog_buttons_have_icons=1
+  #   double_click_interval=400
+  #   gui_effects=@Invalid()
+  #   keyboard_scheme=2
+  #   menus_have_icons=true
+  #   show_shortcuts_in_context_menus=true
+  #   stylesheets=@Invalid()
+  #   toolbutton_style=4
+  #   underline_shortcut=1
+  #   wheel_scroll_lines=3
+  # '';
 }
