ed1be861dc076b08685f706f47782d82ff6d26b5
Jacob Birkett <jacob@birkett.dev>
2022-09-02 05:39:15 -0700

    Workaround swayidle unit service not functioning

--- users/jacob/desktop/default.nix
+++ users/jacob/desktop/default.nix
@@ -4,6 +4,9 @@
   ];
 
   home.packages = [
+    # Idle Event Daemon
+    pkgs.swayidle
+
     # Lock Screen
     pkgs.swaylock-effects
 
@@ -22,7 +25,11 @@
       enable = true;
       hidpi = true;
     };
+
+    # prepend the config with more exec lines,
+    # for setting the wallpaper and starting swayidle
     extraConfig = ''
+      exec-once=${lib.getExe pkgs.swayidle}
       exec=sleep 0.1 && ${config.xdg.configHome}/hypr/wallpaper.sh
     '' + builtins.readFile ./configs/hyprland.conf;
   };
@@ -54,23 +61,43 @@
   # configure swaylock theme
   programs.swaylock.settings = import ./configs/swaylock.nix;
 
-  # runs commands when events from logind or inactive timeout
-  services.swayidle = {
-    enable = true;
-    events = [
-      { event = "before-sleep"; command = "swaylock -f"; }
-      { event = "lock"; command = "swaylock -f"; }
-    ];
-    timeouts = [
-      # lock after 1 minute with a grace of 30 seconds
-      {
-        timeout = 2 * 60;
-        command = "swaylock -f --grace 30";
-      }
-      # for testing
-      { timeout = 5; command = "swaylock -f --grace 10"; }
-    ];
-  };
+  # write the config to the expected location and
+  # execute from hyprland because the systemd service doesn't work
+  xdg.configFile."swayidle/config".text = let
+    # the swayidle path in the nixpkg is wrong
+    # <https://github.com/NixOS/nixpkgs/pull/189452>
+    # swaylock = lib.getExe pkgs.swaylock-effects;
+    swaylock = "${pkgs.swaylock-effects}/bin/swaylock";
+  in ''
+    timeout ${toString (2 * 60)} '${swaylock} -f --grace 30'
+    before-sleep '${swaylock} -f'
+    lock '${swaylock} -f --grace 5 --grace-no-mouse'
+  '';
+
+  # # runs commands when events from logind or inactive timeout
+  # services.swayidle = {
+  #   enable = true;
+  #   events = [
+  #     { event = "before-sleep"; command = "swaylock -f"; }
+  #     { event = "lock"; command = "swaylock -f"; }
+  #   ];
+  #   timeouts = [
+  #     # lock after 1 minute with a grace of 30 seconds
+  #     {
+  #       timeout = 2 * 60;
+  #       command = "swaylock -f --grace 30";
+  #     }
+  #     # for testing
+  #     { timeout = 5; command = "swaylock -f --grace 10"; }
+  #   ];
+  # };
+
+  # # make some modifications to the swayidle service
+  # # WantedBy is sway-session.target by default
+  # systemd.user.services.swayidle.Install.WantedBy =
+  #   lib.mkForce [ "hyprland-session.target" ];
+  # systemd.user.services.swayidle.Service.Restart = "on-failure";
+  # systemd.user.services.swayidle.Service.RestartSec = 5;
 
   # service that auto-mounts storage devices with udisks2
   services.udiskie = {
