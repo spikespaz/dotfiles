3187ad1dfbd717a6f5ef52657b5411a8882cf943
Jacob Birkett <jacob@birkett.dev>
2022-09-09 23:18:10 -0700

    profile: zsh: add plugins

--- users/jacob/programs/default.nix
+++ users/jacob/programs/default.nix
@@ -120,10 +120,28 @@
   };
   zsh = {
     imports = [ dotpkgs.homeManagerModules.zsh-uncruft ];
+
+    programs.starship = {
+      enable = true;
+      settings = {
+        scan_timeout = 100;
+        command_timeout = 1000;
+      };
+    };
+
     programs.zsh-uncruft = {
       enable = true;
       znap.enable = true;
       znap.autoUpdate = true;
+      zshrc.init = ''
+        znap eval starship '${lib.getExe pkgs.starship} init zsh --print-full-init'
+        znap prompt
+      '';
+      zshrc.main = ''
+        znap source marlonrichert/zsh-autocomplete
+        znap source zsh-users/zsh-autosuggestions
+        znap source zsh-users/zsh-syntax-highlighting
+      '';
     };
   };
 
