8258de7db411efcb9420b726437753bc6962d77d
Jacob Birkett <jacob@birkett.dev>
2022-09-12 06:20:40 -0700

    zsh-uncruft: integrations: add aliases

--- packages/zsh-uncruft/integrations.nix
+++ packages/zsh-uncruft/integrations.nix
@@ -1,19 +1,43 @@
 { config, lib, ... }: let
   cfg = config.programs.zsh-uncruft;
 in {
-  imports = [
-    ./znap.nix
-  ];
-
   options = {
     programs.zsh-uncruft = {
       enableIntegrations = lib.mkEnableOption ''
         Enable integrations from other Home Manager modules.
       '';
+
+      enableAliases = lib.mkEnableOption ''
+        Enable aliases defined by other Home Manager modules.
+      '';
     };
   };
 
-  config = lib.mkIf (cfg.enable && cfg.enableIntegrations) {
-    programs.zsh-uncruft.zshrc.postInit = config.programs.zsh.initExtra;
-  };
+  config = lib.mkIf cfg.enable (lib.mkMerge [
+
+    (lib.mkIf cfg.enableIntegrations {
+      programs.zsh-uncruft.zshrc.postInit = ''
+        ### HOME MANAGER EXTRA INITIALIZATION ###
+
+        ${config.programs.zsh.initExtra}
+
+        ### END ###
+      '';
+    })
+
+    (lib.mkIf cfg.enableAliases {
+      programs.zsh-uncruft.zshrc.main = lib.mkAfter
+        (lib.concatStringsSep "\n" (builtins.concatLists [
+          (lib.singleton "### HOME MANAGER ALIASES ###\n")
+          (lib.mapAttrsToList
+            (k: v: "alias ${k}=${lib.escapeShellArg v}")
+            config.programs.zsh.shellAliases)
+          (lib.mapAttrsToList
+            (k: v: "alias -g ${k}=${lib.escapeShellArg v}")
+            config.programs.zsh.shellGlobalAliases)
+          (lib.singleton "\n### END ###")
+        ]));
+    })
+
+  ]);
 }
