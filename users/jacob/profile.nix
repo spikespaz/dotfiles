883ed9595bb9bf3de2516d1cfd040b3b87d1b3d8
Jacob Birkett <jacob@birkett.dev>
2022-09-15 10:06:55 -0700

    me: programs: disable discord

--- users/jacob/profile.nix
+++ users/jacob/profile.nix
@@ -74,7 +74,7 @@ in {
 
     ### COMMUNICATION & MESSAGING ###
     programs.mailspring
-    programs.discord
+    # programs.discord
     programs.webcord
     programs.neochat
     programs.hexchat
