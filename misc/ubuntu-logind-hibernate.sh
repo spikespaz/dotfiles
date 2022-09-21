9fe9075eaf4500639a346e129891c10cf79d0722
Jacob Birkett <jacob@birkett.dev>
2022-09-21 03:05:19 -0700

    misc: add script to enable hibernate with logind

---
+++ misc/ubuntu-logind-hibernate.sh
@@ -0,0 +1,10 @@
+# enable logind hibernation
+cat <<- EOF > /etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla
+	[Enable hibernate]
+	Identity=unix-user:*
+	Action=org.freedesktop.login1.hibernate;org.freedesktop.login1.handle-hibernate-key;org.freedesktop.login1;org.freedesktop.login1.hibernate-multiple-sessions
+	ResultActive=yes
+EOF
+
+# <https://www.linuxuprising.com/2021/08/how-to-enable-hibernation-on-ubuntu.html>
+# <https://forum.zorin.com/t/how-to-enable-hibernate-option-on-zorinos-16-or-other-gnome-distros/15582>
