b0246ef153878d97c48574db13153b14d440af75
Jacob Birkett <jacob@birkett.dev>
2022-09-24 10:32:12 -0700

    me: desk: wl: add seahorse and libsecret to suite

--- users/jacob/desktops/suite.nix
+++ users/jacob/desktops/suite.nix
@@ -4,9 +4,23 @@
     ### DESKTOP ENVIRONMENT ###
     ###########################
 
+    # Qt-Wayland Compatibility
+    # qt6.qtwayland
+    # libsForQt5.qt5.qtwayland
+
     # Device Configuration
     lxqt.pavucontrol-qt  # Pulse Audio Volume Control
     system-config-printer
+
+    # Authentication
+    # libsForQt5.kauth
+    # libsForQt514.polkit-kde-agent
+    # lxqt.lxqt-policykit
+    # polkit_gnome
+    # pantheon.pantheon-agent-polkit
+    # libsForQt5.kwallet
+    gnome.seahorse
+    libsecret
 
     # Dolphin File Manager
     libsForQt5.dolphin
