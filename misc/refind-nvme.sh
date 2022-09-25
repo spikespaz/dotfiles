990d0cdd0ca90bb8435f4be6d7b07c045452164f
Jacob Birkett <jacob@birkett.dev>
2022-09-25 09:44:53 -0700

    misc: refind-nvme: manual install over package script

--- misc/refind-nvme.sh
+++ misc/refind-nvme.sh
@@ -29,6 +29,8 @@ mount --bind  /sys         /target/sys
 
 # chroot and reconfigure bootloader + initramfs
 chroot /target /bin/bash -x <<- EOF
+	shopt -s extglob
+
 	apt-add-repository -y ppa:rodsmith/refind
 	apt update
 	apt upgrade -y
@@ -36,19 +38,50 @@ chroot /target /bin/bash -x <<- EOF
 
 	cd /tmp
 
-	if [ ! -f NvmExpressDxe.efi ];
+	REFIND_VER='0.13.3.1'
+
+	if [ ! -f refind-bin.zip ]
 	then
-		wget 'https://github.com/CloverHackyColor/CloverBootloader/releases/download/5149/CloverV2-5149.zip'
-		7z e ./CloverV2-5149.zip CloverV2/EFI/CLOVER/drivers/off/UEFI/Other/NvmExpressDxe.efi
+		wget \
+			-O refind-bin.zip \
+			"https://cfhcable.dl.sourceforge.net/project/refind/$REFIND_VER/refind-bin-$REFIND_VER.zip"
+		7z x ./refind-bin.zip
 	fi
 
-	refind-install
+	if [ ! -f NvmExpressDxe.efi ]
+	then
+		wget \
+			-O clover.zip \
+			'https://github.com/CloverHackyColor/CloverBootloader/releases/download/5149/CloverV2-5149.zip'
+		7z e ./clover.zip CloverV2/EFI/CLOVER/drivers/off/UEFI/Other/NvmExpressDxe.efi
+	fi
 
-	mkdir -p /boot/efi/EFI/refind/drivers_x64
-	cp NvmExpressDxe.efi /boot/efi/EFI/refind/drivers_x64
+	REFIND_DIR='/boot/efi/EFI/refind'
+	DRIVERS_DIR="$REFIND_DIR/drivers_x64"
+	REFIND_FROM="refind-bin-$REFIND_VER/refind"
+
+	mkdir -p $DRIVERS_DIR
+
+	# copy icons over
+	cp -r $REFIND_FROM/icons $REFIND_DIR
+
+	# copy the boot manager
+	cp $REFIND_FROM/refind_x64.efi $REFIND_DIR
+
+	# copy the default config and modify
+	cp $REFIND_FROM/refind.conf-sample $REFIND_DIR
+	cp $REFIND_FROM/refind.conf-sample $REFIND_DIR/refind.conf
+	sed -i 's/timeout 20/timeout 2/' $REFIND_DIR/refind.conf
+
+	# copy drivers
+	cp $REFIND_FROM/drivers_x64/ext4_x64.efi $DRIVERS_DIR
+	cp NvmExpressDxe.efi $DRIVERS_DIR
+
+	# add the entry to nvram
+	efibootmgr -c -l '\\EFI\\refind\\refind_x64.efi' -L rEFInd
 EOF
 
-clean up the chroot mounts
+# clean up the chroot mounts
 umount /target/sys
 umount /target/proc
 umount /target/dev/pts
