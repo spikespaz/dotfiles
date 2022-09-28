5f1ba4382432c4b3d3174c1309886dfb71ada768
Jacob Birkett <jacob@birkett.dev>
2022-09-27 21:08:09 -0700

    me: hyprland: add script to show volume notifs

--- users/jacob/desktops/hyprland/default.nix
+++ users/jacob/desktops/hyprland/default.nix
@@ -21,16 +21,23 @@
 
     # prepend the config with more exec lines,
     # for starting swayidle
-    extraConfig = lib.concatStringsSep "\n\n" [
-      # polkit agent, raises to root access with gui
-      "exec-once=${lib.getExe pkgs.lxqt.lxqt-policykit}"
-      # allow apps with risen perms after agent to connect to local xwayland
-      "exec-once=${lib.getExe pkgs.xorg.xhost} +local:"
-      # hyprland config, split up
-      (builtins.readFile ./hyprland.conf)
-      (builtins.readFile ./displays.conf)
-      (builtins.readFile ./keybinds.conf)
-      (builtins.readFile ./windowrules.conf)
-    ];
+    extraConfig = (
+      builtins.replaceStrings [
+        "%FUNCTIONS%"
+      ] [
+        "${lib.getExe pkgs.bash} ${./functions.sh}"
+      ]
+      (lib.concatStringsSep "\n\n" [
+        # polkit agent, raises to root access with gui
+        "exec-once=${lib.getExe pkgs.lxqt.lxqt-policykit}"
+        # allow apps with risen perms after agent to connect to local xwayland
+        "exec-once=${lib.getExe pkgs.xorg.xhost} +local:"
+        # hyprland config, split up
+        (builtins.readFile ./hyprland.conf)
+        (builtins.readFile ./displays.conf)
+        (builtins.readFile ./keybinds.conf)
+        (builtins.readFile ./windowrules.conf)
+      ])
+    );
   };
 }
