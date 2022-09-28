6e38e10617cfce9f8a506dcaf6c794c54581bd57
Jacob Birkett <jacob@birkett.dev>
2022-09-28 00:05:14 -0700

    dotpkgs: kbfns: add microphone mute

--- dotpkgs/keyboard-functions/functions.sh
+++ dotpkgs/keyboard-functions/functions.sh
@@ -10,8 +10,12 @@ here="$(realpath "$(dirname $0)")"
 	ICONS='rounded-white'
 [ -z "$VOLUME_TITLE" ] && \
 	VOLUME_TITLE='Default Audio Output'
+[ -z "$MICROPHONE_TITLE" ] && \
+	MICROPHONE_TITLE='Default Audio Input'
 [ -z "$VOLUME_OUTPUT_SINK" ] && \
 	VOLUME_OUTPUT_SINK='@DEFAULT_AUDIO_SINK@'
+[ -z "$INPUT_SOURCE" ] && \
+	INPUT_SOURCE='@DEFAULT_AUDIO_SOURCE@'
 [ -z "$ICONS_DIRECTORY" ] && \
 	ICONS_DIRECTORY="$here/icons"
 [ -z "$MAIN_TEXT_SIZE" ] && \
@@ -20,7 +24,7 @@ here="$(realpath "$(dirname $0)")"
 set -eux
 
 is_muted() {
-	[ -z "$(wpctl get-volume "$VOLUME_OUTPUT_SINK" | grep '[MUTED]')" ] \
+	[ -z "$(wpctl get-volume "$1" | grep '[MUTED]')" ] \
 		|| printf 0
 }
 
@@ -28,10 +32,33 @@ get_volume() {
 	wpctl get-volume "$VOLUME_OUTPUT_SINK" | awk '{print $2}'
 }
 
+input_mute() {
+	wpctl set-mute "$INPUT_SOURCE" toggle
+
+	if [ $(is_muted "$INPUT_SOURCE") ]
+	then
+		icon="$ICONS_DIRECTORY/$ICONS/mic_off_white_36dp.svg"
+		status="Disabled"
+	else
+		icon="$ICONS_DIRECTORY/$ICONS/mic_white_36dp.svg"
+		status="Enabled"
+	fi
+
+	message="<b><span size='$MAIN_TEXT_SIZE'>$status</span></b>"
+
+	notify-send \
+		"$MICROPHONE_TITLE" \
+		"$message" \
+		-u $URGENCY \
+		-t $TIMEOUT \
+		-i "$icon" \
+		-h string:synchronous:change-volume
+}
+
 volume_mute() {
 	wpctl set-mute "$VOLUME_OUTPUT_SINK" toggle
 
-	if [ $(is_muted) ]
+	if [ $(is_muted "$VOLUME_OUTPUT_SINK") ]
 	then
 		icon="$ICONS_DIRECTORY/$ICONS/volume_off_white_36dp.svg"
 		status="Disabled"
@@ -103,7 +130,7 @@ volume_change() {
 
 	wpctl set-volume "$VOLUME_OUTPUT_SINK" $value
 
-	if [ $(is_muted) ]
+	if [ $(is_muted "$VOLUME_OUTPUT_SINK") ]
 	then
 		icon="$ICONS_DIRECTORY/$ICONS/volume_off_white_36dp.svg"
 		status='Disabled'
@@ -133,8 +160,20 @@ if [ $1 = volume ]
 then
 	if [ $2 = mute ]
 	then
-		volume_mute $2
+		volume_mute
+		exit
 	else
 		volume_change $2
+		exit
+	fi
+elif [ $1 = input ]
+then
+	if [ $2 = mute ]
+	then
+		input_mute
+		exit
 	fi
 fi
+
+echo "Unsupported options!"
+exit 11
