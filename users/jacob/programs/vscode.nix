dddd6e1c13b8431c92d9187f83fe4567456751d7
Jacob Birkett <jacob@birkett.dev>
2022-09-28 23:54:22 -0700

    me: vscode: add perl language server extension

--- users/jacob/programs/vscode.nix
+++ users/jacob/programs/vscode.nix
@@ -16,9 +16,15 @@
     # pkief.material-icon-theme
 
     # Language Clients
-    # fractalboy.pls  # Perl
     jnoortheen.nix-ide  # Nix
     rust-lang.rust-analyzer # Rust
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {  # Perl
+      name = "pls";
+      publisher = "fractalboy";
+      version = "0.0.15";
+      sha256 = "sha256-qMXaCxlvGUz7BXl6reFOXtLXV2JMzccxQO4VlvvZOQk=";
+    }
   ];
 
   programs.vscode.userSettings = {
