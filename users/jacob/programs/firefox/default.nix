8b3e1dad333692120175e2d7a5d6137be60e34c6
Jacob Birkett <jacob@birkett.dev>
2022-10-04 04:36:28 -0700

    me: firefox: split config into dir

---
+++ users/jacob/programs/firefox/default.nix
@@ -0,0 +1,55 @@
+{ lib, pkgs, ... }: let
+  # things to do for every user
+  prefab = {
+    settings = {
+      "trailhead.firstrun.didSeeAboutWelcome" = true;
+    };
+  };
+  # set up for userChrome.css
+  userChrome = {
+    settings = {
+      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+      "browser.uidensity" = 1;
+      "ui.prefersReducedMotion" = 1;
+    };
+    userChrome = builtins.readFile ./userChrome.css;
+  };
+in {
+  imports = [
+    ./hosts.nix
+  ];
+
+  programs.firefox.profiles = {
+    "jacob.default" = lib.mkMerge [
+      prefab
+      userChrome
+      {
+        id = 0;
+        isDefault = true;
+        name = "jacob-default";
+      }
+    ];
+  };
+
+  programs.firefox.extensions = let
+    rycee = pkgs.nur.repos.rycee.firefox-addons;
+    bandithedoge = pkgs.nur.repos.bandithedoge.firefoxAddons;
+  in [
+    ### BASICS ###
+    rycee.darkreader
+    rycee.tree-style-tab
+
+    ### PERFORMANCE ###
+    rycee.h264ify
+    rycee.localcdn
+    rycee.auto-tab-discard
+
+    ### BLOCKING ###
+    rycee.ublock-origin
+    # Enable "Annoyances" lists in uBO instead
+    # rycee.i-dont-care-about-cookies
+
+    ### GITHUB ###
+    bandithedoge.gitako
+  ];
+}
