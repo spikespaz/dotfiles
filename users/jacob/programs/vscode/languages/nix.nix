d9f030f972d31a86bc5a308fbe7d5a55d75c4fcc
Jacob Birkett <jacob@birkett.dev>
2022-10-06 05:59:40 -0700

    me: vscode: add alejandra

--- users/jacob/programs/vscode/languages/nix.nix
+++ users/jacob/programs/vscode/languages/nix.nix
@@ -5,15 +5,19 @@
 }: {
   home.packages = with pkgs; [
     nil
+    alejandra
   ];
 
   programs.vscode.extensions = with pkgs.vscode-extensions; [
     jnoortheen.nix-ide
+    kamadorueda.alejandra
   ];
 
   programs.vscode.userSettings = {
     "nix.enableLanguageServer" = true;
     "nix.serverPath" = lib.getExe pkgs.nil;
+    # "nix.formatterPath" = lib.getExe pkgs.alejandra;
+    "alejandra.program" = lib.getExe pkgs.alejandra;
     "[nix]" = {
       # appears to be buggy at the moment
       "editor.stickyScroll.enabled" = false;
