60608d69f8c6a8fedbf084294565c57129675ca2
Jacob Birkett <jacob@birkett.dev>
2022-10-15 19:23:14 -0700

    systems: tpad: organize configuration.nix into groups

--- systems/jacob-thinkpad/configuration.nix
+++ systems/jacob-thinkpad/configuration.nix
@@ -1,92 +1,132 @@
 args @ {
   config,
   pkgs,
+  lib,
   ...
-}: {
+}:
+lib.mkMerge [
   #################
   ### NIX SETUP ###
   #################
+  {
+    # only change if you want to fix breaking changes
+    system.stateVersion = "22.05";
 
-  nix.settings = {
-    # allow the flake settings
-    # accept-flake-config = true;
-    # use four cores for enableParallelBuilding
-    cores = 4;
-    # allow sudo users to mark the following values as trusted
-    trusted-users = ["root" "@wheel"];
-    # only allow sudo users to manage the nix store
-    allowed-users = ["@wheel"];
-    # enable new nix command and flakes
-    extra-experimental-features = [
-      "flakes"
-      "nix-command"
-    ];
+    # set up garbage collection to run weekly,
+    # removing unused packages after seven days
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
 
-    # TODO: Make this Flake nixConfig
-    # continue building derivations if one fails
-    keep-going = true;
-    # show more log lines for failed builds
-    log-lines = 20;
-    # instances of cachix for package derivations
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://fog.cachix.org"
-      "https://webcord.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "fog.cachix.org-1:FAxiA6qMLoXEUdEq+HaT24g1MjnxdfygrbrLDBp6U/s="
-      "webcord.cachix.org-1:l555jqOZGHd2C9+vS8ccdh8FhqnGe8L78QrHNn+EFEs="
-    ];
-  };
+    nix.settings = {
+      # allow the flake settings
+      # accept-flake-config = true;
+      # use four cores for enableParallelBuilding
+      cores = 4;
+      # allow sudo users to mark the following values as trusted
+      trusted-users = ["root" "@wheel"];
+      # only allow sudo users to manage the nix store
+      allowed-users = ["@wheel"];
+      # enable new nix command and flakes
+      extra-experimental-features = [
+        "flakes"
+        "nix-command"
+      ];
 
-  # set up garbage collection to run weekly,
-  # removing unused packages after seven days
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
+      # TODO: Make this Flake nixConfig
+      # continue building derivations if one fails
+      keep-going = true;
+      # show more log lines for failed builds
+      log-lines = 20;
+      # instances of cachix for package derivations
+      extra-substituters = [
+        "https://nix-community.cachix.org"
+        "https://fog.cachix.org"
+        "https://webcord.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "fog.cachix.org-1:FAxiA6qMLoXEUdEq+HaT24g1MjnxdfygrbrLDBp6U/s="
+        "webcord.cachix.org-1:l555jqOZGHd2C9+vS8ccdh8FhqnGe8L78QrHNn+EFEs="
+      ];
+    };
+  }
+  ################
+  ### HOSTNAME ###
+  ################
+  {
+    networking = {
+      hostName = "jacob-thinkpad";
+      hostId = builtins.substring 0 8 (
+        builtins.hashString "md5" config.networking.hostName
+      );
 
-  console = {
-    keyMap = "us";
-    font = "Lat2-Terminus16";
-    earlySetup = true;
-  };
+      networkmanager.enable = true;
+    };
+  }
 
   ##########################
   ### BOOT CONFIGURATION ###
   ##########################
+  {
+    # systemd pivots to ramfs on shutdown
+    # this is so that the root fs can be unmounted safely
+    # it is not worth my time, I live on the edge
+    systemd.shutdownRamfs.enable = false;
 
-  # systemd pivots to ramfs on shutdown
-  # this is so that the root fs can be unmounted safely
-  # it is not worth my time, I live on the edge
-  systemd.shutdownRamfs.enable = false;
-
-  boot = {
-    kernelModules = ["kvm-amd" "acpi_call"];
-    extraModulePackages = with config.boot.kernelPackages; [
-      acpi_call
-    ];
-
-    # configure plymouth theme
-    # <https://github.com/adi1090x/plymouth-themes>
-    plymouth = let
-      pack = 3;
-      theme = "hud_3";
-    in {
-      enable = true;
-      themePackages = [
-        (pkgs.plymouth-themes.override {inherit pack theme;})
+    boot = {
+      kernelModules = ["kvm-amd" "acpi_call"];
+      extraModulePackages = with config.boot.kernelPackages; [
+        acpi_call
       ];
-      inherit theme;
+
+      kernelParams = [];
+
+      initrd.kernelModules = ["amdgpu" "nvme"];
+      initrd.availableKernelModules = [
+        "ehci_pci"
+        "xhci_pci"
+        "usb_storage"
+        "usbhid"
+        "sd_mod"
+        "rtsx_pci_sdmmc"
+      ];
+
+      initrd.systemd.strip = false;
+      initrd.systemd.enable = true;
+
+      loader = {
+        systemd-boot.enable = true;
+        systemd-boot.editor = false;
+        systemd-boot.configurationLimit = 5;
+
+        # need to hold space to get the boot menu to appear
+        timeout = 0;
+
+        efi.efiSysMountPoint = "/boot";
+        efi.canTouchEfiVariables = true;
+      };
+    };
+  }
+
+  ###########################
+  ### PLYMOUTH & GRAPHICS ###
+  ###########################
+  {
+    # font for log text & tty
+    console = {
+      keyMap = "us";
+      font = "Lat2-Terminus16";
+      earlySetup = true;
     };
 
     # make the boot quiet
-    consoleLogLevel = 3;
-    initrd.verbose = false;
+    boot.consoleLogLevel = 3;
+    boot.initrd.verbose = false;
 
-    kernelParams = [
+    boot.kernelParams = [
       # ensures that amdgpu is loaded over radeon
       "amdgpu"
       # allows the backlight to be controlled via software
@@ -104,208 +144,209 @@ args @ {
       # lower the udev log level to show only errors or worse
       "rd.udev.log_level=3"
       # disable the cursor in vt to get a black screen during intermissions
+      # TODO turn back on in tty
       "vt.global_cursor_default=0"
     ];
 
-    initrd.kernelModules = ["amdgpu" "nvme"];
-    initrd.availableKernelModules = [
-      "ehci_pci"
-      "xhci_pci"
-      "usb_storage"
-      "usbhid"
-      "sd_mod"
-      "rtsx_pci_sdmmc"
-    ];
-
-    initrd.systemd.strip = false;
-    initrd.systemd.enable = true;
-
-    loader = {
-      systemd-boot.enable = true;
-      systemd-boot.editor = false;
-      systemd-boot.configurationLimit = 5;
-
-      # need to hold space to get the boot menu to appear
-      timeout = 0;
-
-      efi.efiSysMountPoint = "/boot";
-      efi.canTouchEfiVariables = true;
+    # configure plymouth theme
+    # <https://github.com/adi1090x/plymouth-themes>
+    boot.plymouth = let
+      pack = 3;
+      theme = "hud_3";
+    in {
+      enable = true;
+      themePackages = [
+        (pkgs.plymouth-themes.override {inherit pack theme;})
+      ];
+      inherit theme;
     };
-  };
+  }
 
   ###########################
   ### HARDWARE & FIRMWARE ###
   ###########################
+  {
+    ### SERVICES: FIRMWARE ###
+    # firmware updater for machine hardware
+    services.fwupd.enable = true;
 
-  networking = {
-    hostName = "jacob-thinkpad";
-    hostId = builtins.substring 0 8 (
-      builtins.hashString "md5" config.networking.hostName
-    );
+    ### FIRMWARE ###
+    hardware = {
+      # enable proprietary firmware that is still redistributable
+      # required for some hardware, drivers contain proprietary blobs
+      enableRedistributableFirmware = true;
 
-    networkmanager.enable = true;
-  };
+      # update processor firmware by loading from memory at boot
+      cpu.amd.updateMicrocode = true;
 
-  hardware = {
-    # enable proprietary firmware that is still redistributable
-    # required for some hardware, drivers contain proprietary blobs
-    enableRedistributableFirmware = true;
+      # wifi adapter
+      firmware = [pkgs.rtw89-firmware];
 
-    # update processor firmware by loading from memory at boot
-    cpu.amd.updateMicrocode = true;
+      # enable bluetooth but turn off power by default
+      bluetooth.enable = true;
+      bluetooth.powerOnBoot = false;
 
-    # wifi adapter
-    firmware = [pkgs.rtw89-firmware];
+      # enable opengl just in case the compositor doesn't
+      opengl.enable = true;
+      opengl.driSupport32Bit = true;
 
-    # enable bluetooth but turn off power by default
-    bluetooth.enable = true;
-    bluetooth.powerOnBoot = false;
+      # enable the lenovo trackpoint (default) but decrease sensitivity
+      trackpoint.enable = true;
+      trackpoint.speed = 85;
+    };
+  }
 
-    # enable opengl just in case the compositor doesn't
-    opengl.enable = true;
-    opengl.driSupport32Bit = true;
+  ################
+  ### SERVICES ###
+  ################
+  {
+    ### SERVICES: AUTO MOUNT ###
 
-    # enable the lenovo trackpoint (default) but decrease sensitivity
-    trackpoint.enable = true;
-    trackpoint.speed = 85;
-  };
+    # storage daemon required for udiskie auto-mount
+    services.udisks2.enable = true;
 
-  #######################
-  ### SYSTEM SERVICES ###
-  #######################
+    ### SERVICES: WIRELESS ###
 
-  # audio and video drivers with legacy alsa, jack, and pulse support
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-  };
+    # bluetooth
+    services.blueman.enable = true;
 
-  # enable cups and add some drivers for common printers
-  services.printing = {
-    enable = true;
-    drivers = with pkgs; [
-      gutenprint
-      hplip
-    ];
-  };
+    ### SERVICES: LOCATION ###
 
-  # required for network discovery of printers
-  services.avahi = {
-    enable = true;
-    # resolve .local domains for printers
-    nssmdns = true;
-  };
+    location.provider = "geoclue2";
+    services.geoclue2.enable = true;
+  }
 
-  location.provider = "geoclue2";
-  services.geoclue2.enable = true;
+  #########################
+  ### SERVIES: PRINTING ###
+  #########################
+  {
+    # audio and video drivers with legacy alsa, jack, and pulse support
+    services.pipewire = {
+      enable = true;
+      pulse.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+    };
 
-  # storage daemon required for udiskie auto-mount
-  services.udisks2.enable = true;
+    # enable cups and add some drivers for common printers
+    services.printing = {
+      enable = true;
+      drivers = with pkgs; [
+        gutenprint
+        hplip
+      ];
+    };
 
-  # bluetooth
-  services.blueman.enable = true;
+    # required for network discovery of printers
+    services.avahi = {
+      enable = true;
+      # resolve .local domains for printers
+      nssmdns = true;
+    };
+  }
 
-  # firmware updater for machine hardware
-  services.fwupd.enable = true;
+  #############################
+  ### SERVICES: APP SANDBOX ###
+  ############################
+  {
+    ### SERVICES: FLATPAK ###
+    services.flatpak.enable = true;
 
-  services.flatpak.enable = true;
+    # cross-desktop group; they make specifications
+    # for what ceratin environment variables should be
+    # <https://github.com/fufexan/dotfiles/blob/785b65436f5849a8dea175d967d901159f689edd/modules/desktop.nix#L153>
+    # not just flatpak, also useful on wayland
+    xdg.portal = {
+      enable = true;
+      wlr.enable = true;
+      # lxqt.enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        # xdg-desktop-portal-gnome
+        # libsForQt5.xdg-desktop-portal-kde
+      ];
+    };
+  }
 
   ###########################
   ### DESKTOP ENVIRONMENT ###
   ###########################
+  {
+    # policy kit;
+    # communication between unpriviledged and proviledged processes
+    security.polkit.enable = true;
 
-  # cross-desktop group; they make specifications
-  # for what ceratin environment variables should be
-  # <https://github.com/fufexan/dotfiles/blob/785b65436f5849a8dea175d967d901159f689edd/modules/desktop.nix#L153>
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    # lxqt.enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      # xdg-desktop-portal-gnome
-      # libsForQt5.xdg-desktop-portal-kde
-    ];
-  };
+    # auth
+    services.gnome.gnome-keyring.enable = true;
 
-  # policy kit;
-  # communication between unpriviledged and proviledged processes
-  security.polkit.enable = true;
+    # enable fingerprint sensor
+    services.fprintd.enable = true;
 
-  # auth
-  services.gnome.gnome-keyring.enable = true;
+    # <https://github.com/swaywm/swaylock/issues/61>
+    security.pam.services.swaylock.text = ''
+      auth sufficient pam_unix.so try_first_pass likeauth nullok
+      auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
+      auth include login
+    '';
 
-  # enable fingerprint sensor
-  services.fprintd.enable = true;
+    # registry for linux, thanks to gnome
+    programs.dconf.enable = true;
 
-  # <https://github.com/swaywm/swaylock/issues/61>
-  security.pam.services.swaylock.text = ''
-    auth sufficient pam_unix.so try_first_pass likeauth nullok
-    auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
-    auth include login
-  '';
+    # locale and timezone
+    time.timeZone = "America/Phoenix";
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  # registry for linux, thanks to gnome
-  programs.dconf.enable = true;
+    # default packages that are good to have on any system
+    environment.systemPackages = import ./packages.nix args;
 
-  # locale and timezone
-  time.timeZone = "America/Phoenix";
-  i18n.defaultLocale = "en_US.UTF-8";
+    # allow users to mount fuse filesystems with allow_other
+    programs.fuse.userAllowOther = true;
 
-  # default packages that are good to have on any system
-  environment.systemPackages = import ./packages.nix args;
+    ### DEFAULT FONTS ###
 
-  # allow users to mount fuse filesystems with allow_other
-  programs.fuse.userAllowOther = true;
-
-  ######################
-  ### VIRTUALIZATION ###
-  ######################
-
-  # virtualisation.spiceUSBRedirection.enable = true;
-  virtualisation.libvirtd = {
-    enable = true;
-    onBoot = "ignore";
-    qemu.swtpm.enable = true;
-    qemu.ovmf.packages = [pkgs.OVMFFull.fd];
-  };
-
-  #####################
-  ### DEFAULT FONTS ###
-  #####################
-
-  fonts = {
-    fontconfig.enable = true;
-    fontDir.enable = true;
-    fonts = with pkgs; [
-      (pkgs.ttf-ms-win11.override {acceptEula = true;})
-      noto-fonts
-      noto-fonts-extra
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-emoji
-      open-sans
-      ubuntu_font_family
-    ];
-  };
+    fonts = {
+      fontconfig.enable = true;
+      fontDir.enable = true;
+      fonts = with pkgs; [
+        (pkgs.ttf-ms-win11.override {acceptEula = true;})
+        noto-fonts
+        noto-fonts-extra
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-emoji
+        open-sans
+        ubuntu_font_family
+      ];
+    };
+  }
 
   #####################
   ### USERS CONFIGS ###
   #####################
+  {
+    # enable completions for system packages
+    environment.pathsToLink = ["/share/zsh" "/share/bash-completion"];
 
-  # enable completions for system packages
-  environment.pathsToLink = ["/share/zsh" "/share/bash-completion"];
-
-  users.users = {
-    jacob = {
-      description = "Jacob Birkett";
-      isNormalUser = true;
-      initialPassword = "password";
-      extraGroups = ["networkmanager" "wheel" "libvirtd"];
+    users.users = {
+      jacob = {
+        description = "Jacob Birkett";
+        isNormalUser = true;
+        initialPassword = "password";
+        extraGroups = ["networkmanager" "wheel" "libvirtd"];
+      };
     };
-  };
+  }
 
-  system.stateVersion = "22.05";
-}
+  ######################
+  ### VIRTUALIZATION ###
+  ######################
+  {
+    # virtualisation.spiceUSBRedirection.enable = true;
+    virtualisation.libvirtd = {
+      enable = true;
+      onBoot = "ignore";
+      qemu.swtpm.enable = true;
+      qemu.ovmf.packages = [pkgs.OVMFFull.fd];
+    };
+  }
+]
