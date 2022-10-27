c00486fec37166cef783975427e70d74290861b3
spikespaz <jacob@birkett.dev>
2022-10-27 15:33:26 +0000

    sys: tpad: move bootloader config to own module

--- systems/jacob-thinkpad/configuration.nix
+++ systems/jacob-thinkpad/configuration.nix
@@ -67,50 +67,6 @@ lib.mkMerge [
     };
   }
 
-  ##########################
-  ### BOOT CONFIGURATION ###
-  ##########################
-  {
-    # systemd pivots to ramfs on shutdown
-    # this is so that the root fs can be unmounted safely
-    # it is not worth my time, I live on the edge
-    systemd.shutdownRamfs.enable = false;
-
-    boot = {
-      kernelModules = ["kvm-amd" "acpi_call"];
-      extraModulePackages = with config.boot.kernelPackages; [
-        acpi_call
-      ];
-
-      kernelParams = [];
-
-      initrd.kernelModules = ["amdgpu" "nvme"];
-      initrd.availableKernelModules = [
-        "ehci_pci"
-        "xhci_pci"
-        "usb_storage"
-        "usbhid"
-        "sd_mod"
-        "rtsx_pci_sdmmc"
-      ];
-
-      initrd.systemd.strip = false;
-      initrd.systemd.enable = true;
-
-      loader = {
-        systemd-boot.enable = true;
-        systemd-boot.editor = false;
-        systemd-boot.configurationLimit = 5;
-
-        # need to hold space to get the boot menu to appear
-        timeout = 0;
-
-        efi.efiSysMountPoint = "/boot";
-        efi.canTouchEfiVariables = true;
-      };
-    };
-  }
-
   ###########################
   ### PLYMOUTH & GRAPHICS ###
   ###########################
