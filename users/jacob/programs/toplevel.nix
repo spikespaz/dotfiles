f6e650d144e6445c9c7a30a288f7aa62478f6da0
Jacob Birkett <jacob@birkett.dev>
2022-11-08 02:35:54 -0700

    me: progs: discord: declaratively create settings

--- users/jacob/programs/toplevel.nix
+++ users/jacob/programs/toplevel.nix
@@ -23,22 +23,47 @@
         nss = pkgs.nss_latest;
       })
     ];
+
+    xdg.configFile."discord/settings.json".text = let
+      bdAddons = pkgs.fetchFromGitHub {
+        owner = "mwittrien";
+        repo = "BetterDiscordAddons";
+        rev = "8627bb7f71c296d9e05d82538d3af8f739f131dc";
+        sha256 = "sha256-Dn6igqL0GvaOcTFZOtQOxuk0ikrWxyDZ41tNsJXJAxc=";
+      };
+    in
+      builtins.toJSON {
+        openasar = {
+          setup = true;
+          cmdPreset = "balanced";
+          quickstart = true;
+          css = builtins.readFile "${bdAddons}/Themes/DiscordRecolor/DiscordRecolor.theme.css";
+        };
+        SKIP_HOST_UPDATE = true;
+        IS_MAXIMIZED = false;
+        IS_MINIMIZED = false;
+        trayBalloonShown = false;
+      };
   };
-  webcord = {pkgs, hmModules, ...}: {
+  webcord = {
+    pkgs,
+    hmModules,
+    ...
+  }: {
     imports = [hmModules.webcord];
 
     programs.webcord = {
       enable = true;
       themes = let
-        repo = pkgs.fetchFromGitHub {
+        bdAddons = pkgs.fetchFromGitHub {
           owner = "mwittrien";
           repo = "BetterDiscordAddons";
           rev = "8627bb7f71c296d9e05d82538d3af8f739f131dc";
           sha256 = "sha256-Dn6igqL0GvaOcTFZOtQOxuk0ikrWxyDZ41tNsJXJAxc=";
         };
       in {
-        DiscordRecolor = "${repo}/Themes/DiscordRecolor/DiscordRecolor.theme.css";
-        SettingsModal = "${repo}/Themes/SettingsModal/SettingsModal.theme.css";
+        DiscordRecolor = "${bdAddons}/Themes/DiscordRecolor/DiscordRecolor.theme.css";
+        SettingsModal = "${bdAddons}/Themes/SettingsModal/SettingsModal.theme.css";
       };
     };
   };
