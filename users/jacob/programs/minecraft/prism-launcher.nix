ae548077328dd857e4a661c0e55167e45edb6b94
Jacob Birkett <jacob@birkett.dev>
2022-11-08 20:42:32 -0700

    me: mc: prism: add graalvm java 8 jre

--- users/jacob/programs/minecraft/prism-launcher.nix
+++ users/jacob/programs/minecraft/prism-launcher.nix
@@ -1,9 +1,51 @@
-{pkgs, ...}: let
-  # just in case the project gets forked again
-  launcherName = "PrismLauncher";
+{
+  pkgs,
+  nixpkgs,
+  ...
+}: let
+  # maybe this will change in the future?
+  dataDir = "PolyMC";
+  # graalvm for java 8 is not in nixpkgs,
+  # there are a few open issues but this seems simplest
+  mkGraal = opts:
+    pkgs.callPackage (import "${nixpkgs}/pkgs/development/compilers/graalvm/community-edition/mkGraal.nix" opts) {
+      Foundation = null;
+    };
+  # TODO clean up the nixpkgs source code,
+  # only run what is necessary and remove the stupidity here
+  graalvm8-ce =
+    (mkGraal {
+      config = {
+        x86_64-linux = {
+          products = [
+            "graalvm-ce"
+            # "native-image-installable-svm"
+          ];
+          arch = "linux-amd64";
+        };
+      };
+      defaultVersion = "21.3.1";
+      javaVersion = "8";
+      sourcesPath = ./graalvm8-ce-sources.json;
+    })
+    .overrideAttrs (old: {
+      # why do I have to do this? the native image is disabled
+      preInstall = ''
+        mkdir -p $out/lib/svm/clibraries/linux-amd64
+      '';
+
+      # nixpkgs needs to be audited badly,
+      # we are missing the pre- and post- hooks
+      installPhase = ''
+        runHook preInstall
+        ${old.installPhase}
+        runHook postInstall
+      '';
+    });
 in {
   home.packages = [
     # TODO make a pull request
+    # this is fixed on Prism Launcher
     # (pkgs.polymc.overrideAttrs (old: {
     #   buildInputs =
     #     old.buildInputs
@@ -11,17 +53,13 @@ in {
     #       pkgs.libsForQt5.qt5.qtwayland
     #     ];
     # }))
-    pkgs.prismlauncher
+    (pkgs.prismlauncher.override {enableLTO = true;})
   ];
 
-  xdg.dataFile."${launcherName}/java/graalvm-ce-java17".source = pkgs.graalvm17-ce;
-  xdg.dataFile."${launcherName}/java/graalvm-ce-java11".source = pkgs.graalvm11-ce;
-  # TODO I don't know why but this one polymc doesn't like
-  xdg.dataFile."${launcherName}/java/graalvm-ce-java8".source = builtins.fetchTarball {
-    url = "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.3.1/graalvm-ce-java8-linux-amd64-21.3.1.tar.gz";
-    sha256 = "sha256:1n6bnvf51gld4hc7dkikmlaqcxxkspjkpck9w912pvjllpi4k3p9";
-  };
-  xdg.dataFile."${launcherName}/java/zulu-java8".source = pkgs.zulu8;
+  xdg.dataFile."${dataDir}/java/graalvm-ce-java17".source = pkgs.graalvm17-ce;
+  xdg.dataFile."${dataDir}/java/graalvm-ce-java11".source = pkgs.graalvm11-ce;
+  xdg.dataFile."${dataDir}/java/graalvm-ce-java8".source = graalvm8-ce;
+  xdg.dataFile."${dataDir}/java/zulu-java8".source = pkgs.zulu8;
 }
 # Overriding PolyMC because of:
 #
