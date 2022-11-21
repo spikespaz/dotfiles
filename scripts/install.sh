b266bbd1900611f823838968fde3d97cfa178599
Jacob Birkett <jacob@birkett.dev>
2022-11-20 22:10:52 -0700

    scripts: install: wrap commands robustly

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,60 @@
-#! /bin/sh
+#! /usr/bin/env bash
 set -eu
 
-nixos-install --flake "path:$(pwd)#"
+nix () {
+	/run/current-system/sw/bin/nix \
+		--extra-experimental-features nix-command \
+		--extra-experimental-features flakes \
+		"$@"
+}
 
-if grep -qs '/mnt ' /proc/mounts; then
-	umount -Rl /mnt
+flake_path=''
+host_name="$(hostname)"
+root_dir='/mnt'
+# unstable=1
+passthru=()
+
+if [ "$#" -eq 1 ]; then
+	host_name="$1"
+	shift
+elif [ "$#" -gt 1 ]; then
+	while [ "$#" -gt 0 ]; do
+		case "$1" in
+			--flake)
+				flake_path="$2"
+				shift 2
+				;;
+			-h|--host)
+				host_name="$2"
+				shift 2
+				;;
+			-r|--root)
+				root_dir="$2"
+				shift 2
+				;;
+			--)
+				shift
+				passthru=($@)
+				break
+				;;
+			*)
+				echo "Unrecognized option $1"
+				exit 1
+				;;
+		esac
+	done
+# else
+# 	echo 'Requires at least one argument, the name of the attribute in #nixosConfigurations to build.'
 fi
 
-zpool export -a
+here="$(realpath "$(dirname "$0")")"
+
+if [ -z "$flake_path" ]; then
+	flake_path="$(git -C "$here" rev-parse --show-toplevel)"
+fi
+
+nixos-install \
+	--root "$root_dir" \
+	--flake "path:$flake_path#$host_name" \
+	--no-root-password \
+	"${passthru[@]}"
