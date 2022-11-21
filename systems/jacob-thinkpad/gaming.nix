cd9d004bfb306cf73ab87afb557f1b014e4c9d4a
Jacob Birkett <jacob@birkett.dev>
2022-11-20 22:14:20 -0700

    sys: tpad: add run-game wrapper script (renice)

---
+++ systems/jacob-thinkpad/gaming.nix
@@ -0,0 +1,34 @@
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  runGameScriptBin = pkgs.writeShellScriptBin "run-game" ''
+    set -eu
+    if [ "$(id -u)" -eq 0 ]; then
+      ${pkgs.util-linux}/bin/renice -n -19 -p "$1"
+    else
+      set -m
+      "$@" &
+      pid=$!
+      /run/wrappers/bin/sudo "$0" $pid
+      fg 1
+    fi
+  '';
+in {
+  environment.systemPackages = [
+    runGameScriptBin
+  ];
+
+  security.sudo.extraRules = [
+    {
+      users = ["jacob"];
+      commands = [
+        {
+          command = lib.getExe runGameScriptBin;
+          options = ["NOPASSWD"];
+        }
+      ];
+    }
+  ];
+}
