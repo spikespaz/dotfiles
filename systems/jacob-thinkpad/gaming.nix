efa467f00b6c3fc1069dceb8c6767a47c7825637
Jacob Birkett <jacob@birkett.dev>
2022-11-22 02:31:09 -0700

    systems: tpad: gaming: enable steam

--- systems/jacob-thinkpad/gaming.nix
+++ systems/jacob-thinkpad/gaming.nix
@@ -31,4 +31,12 @@ in {
       ];
     }
   ];
+
+  programs.steam = {
+    enable = true;
+    # Open ports in the firewall for Steam Remote Play
+    remotePlay.openFirewall = true;
+    # Open ports in the firewall for Source Dedicated Server
+    dedicatedServer.openFirewall = true;
+  };
 }
