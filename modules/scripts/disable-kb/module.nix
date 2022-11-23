e3f2bdaef38069f064263a4dbe2d4a1772ac9bb1
Jacob Birkett <jacob@birkett.dev>
2022-11-23 15:43:26 -0700

    modules: disable-kb: add module to set udev rules

---
+++ modules/scripts/disable-kb/module.nix
@@ -0,0 +1,75 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib) types;
+  deviceFds = map (name: "/dev/${name}") (builtins.attrNames cfg.disableDevices);
+  scriptBin = pkgs.writeShellScriptBin "wayland-disable-keyboard" ''
+    export PATH='/run/wrappers/bin:${lib.makeBinPath (with pkgs; [
+      bash
+      coreutils
+      dbus
+      libnotify
+      evtest
+    ])}'
+    export DISABLE_DEVICES='${lib.concatStringsSep ":" deviceFds}'
+    ${builtins.readFile ./disable_kb.sh}
+  '';
+  cfg = config.desktopScripts.disableKeyboard;
+in {
+  options = {
+    desktopScripts.disableKeyboard = {
+      enable = lib.mkEnableOption (lib.mdDoc '''');
+      allowedUsers = lib.mkOption {
+        type = types.listOf types.singleLineStr;
+        default = [];
+        description = lib.mdDoc '''';
+        example = lib.literalExpression '''';
+      };
+      allowedGroups = lib.mkOption {
+        type = types.listOf types.singleLineStr;
+        default = [];
+        description = lib.mdDoc '''';
+        example = lib.literalExpression '''';
+      };
+      disableDevices = lib.mkOption {
+        type = types.attrsOf types.attrs;
+        default = [];
+        description = lib.mdDoc '''';
+        example = lib.literalExpression '''';
+      };
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [scriptBin];
+
+    services.udev.extraRules = (
+      lib.concatStringsSep "\n" (
+        builtins.attrValues (
+          builtins.mapAttrs (name: {
+            product,
+            vendor,
+          }: ''
+            SUBSYSTEMS=="input", ATTRS{id/product}=="${product}", ATTRS{id/vendor}=="${vendor}", SYMLINK+="${name}"
+          '')
+          cfg.disableDevices
+        )
+      )
+    );
+
+    security.sudo.extraRules = [
+      {
+        users = cfg.allowedUsers;
+        groups = cfg.allowedGroups;
+        commands = [
+          {
+            command = lib.getExe scriptBin;
+            options = ["NOPASSWD"];
+          }
+        ];
+      }
+    ];
+  };
+}
--- systems/jacob-thinkpad/configuration.nix
+++ systems/jacob-thinkpad/configuration.nix
@@ -1,4 +1,5 @@
 args @ {
+  flake,
   config,
   pkgs,
   lib,
@@ -239,6 +240,67 @@ lib.mkMerge [
         open-sans
         ubuntu_font_family
       ];
+    };
+  }
+
+  ####################################
+  ### DESKTOP ENVIRONMENT: WAYLAND ###
+  ####################################
+  {
+    # <https://github.com/swaywm/swaylock/issues/61>
+    # security.pam.services.swaylock.text = ''
+    #   auth sufficient ${pkgs.fprintd-grosshack}/lib/security/pam_fprintd_grosshack.so
+    #   auth sufficient pam_unix.so try_first_pass nullok
+    # '';
+
+    # imports = [flake.modules.scripts.disable-kb.module];
+    desktopScripts.disableKeyboard = {
+      enable = true;
+      allowedUsers = ["jacob"];
+      # Show all event devices:
+      # $ sudo evtest
+      # Get information about a device:
+      # $ udevadm info -a /dev/input/eventXX
+      # Test by blocking a device:
+      # $ sudo evtest --grab /dev/input/eventXX
+      disableDevices = {
+        # "AT Translated Set 2 keyboard"
+        # Laptop Keyboard
+        "thinkpad/keyboard" = {
+          product = "0001";
+          vendor = "0001";
+        };
+        # "ThinkPad Extra Buttons"
+        # Laptop Special Function Keys
+        "thinkpad/extra-buttons" = {
+          product = "5054";
+          vendor = "17aa";
+        };
+        # "TPPS/2 Elan TrackPoint"
+        # TrackPoint and Touchpad Buttons
+        "thinkpad/trackpoint" = {
+          product = "000a";
+          vendor = "0002";
+        };
+        # "SynPS/2 Synaptics TouchPad"
+        # Laptop Touchpad
+        "thinkpad/touchpad" = {
+          product = "0007";
+          vendor = "0002";
+        };
+        # # "Power Button"
+        # # Power/Sleep Button
+        # "thinkpad/power-button" = {
+        #   product = "0001";
+        #   vendor = "0000";
+        # };
+        # "Lid Switch"
+        # Laptop Close Switch
+        "thinkpad/lid-switch" = {
+          product = "0005";
+          vendor = "0000";
+        };
+      };
     };
   }
 
