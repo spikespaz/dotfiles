c8139dea522aac5eb4df9035030deb560d9c6160
Jacob Birkett <jacob@birkett.dev>
2022-11-24 00:42:22 -0700

    modules: disable-kb: module: fix for new scripts

--- modules/scripts/disable-kb/module.nix
+++ modules/scripts/disable-kb/module.nix
@@ -5,29 +5,26 @@
   ...
 }: let
   inherit (lib) types;
-  deviceFds = map (name: "/dev/${name}") (builtins.attrNames cfg.disableDevices);
-  scriptBin = pkgs.writeShellScriptBin "wayland-disable-keyboard" ''
-    export PATH='/run/wrappers/bin:${lib.makeBinPath (with pkgs; [
-      bash
-      coreutils
-      dbus
-      libnotify
-      evtest
-    ])}'
-    export DISABLE_DEVICES='${lib.concatStringsSep ":" deviceFds}'
-    ${
-      builtins.replaceStrings [
-        "toggle_script=\"$here/toggle_kb.sh\""
-      ] [
-        "toggle_script='${./toggle_kb.sh}'"
-      ] (builtins.readFile ./disable_kb.sh)
-    }
-  '';
-  cfg = config.desktopScripts.disableKeyboard;
+  defaultPackage = pkgs.callPackage ./package.nix {};
+  cfg = config.programs.wayland-disable-keyboard;
 in {
   options = {
-    desktopScripts.disableKeyboard = {
+    programs.wayland-disable-keyboard = {
       enable = lib.mkEnableOption (lib.mdDoc '''');
+      package = lib.mkOption {
+        type = types.package;
+        default = defaultPackage;
+        description = lib.mdDoc '''';
+        example = lib.literalExpression ''
+          pkgs.wayland-disable-keyboard
+        '';
+      };
+      disableDevices = lib.mkOption {
+        type = types.attrsOf types.attrs;
+        default = [];
+        description = lib.mdDoc '''';
+        example = lib.literalExpression '''';
+      };
       allowedUsers = lib.mkOption {
         type = types.listOf types.singleLineStr;
         default = [];
@@ -40,42 +37,43 @@ in {
         description = lib.mdDoc '''';
         example = lib.literalExpression '''';
       };
-      disableDevices = lib.mkOption {
-        type = types.attrsOf types.attrs;
-        default = [];
-        description = lib.mdDoc '''';
-        example = lib.literalExpression '''';
-      };
     };
   };
-  config = lib.mkIf cfg.enable {
-    environment.systemPackages = [scriptBin];
+  config = lib.mkIf cfg.enable (
+    let
+      deviceFds = map (name: "/dev/${name}") (builtins.attrNames cfg.disableDevices);
+      wrappedPackage = cfg.package.override {
+        disableDevices = deviceFds;
+      };
+    in {
+      environment.systemPackages = [wrappedPackage];
 
-    services.udev.extraRules = (
-      lib.concatStringsSep "\n" (
-        builtins.attrValues (
-          builtins.mapAttrs (name: {
-            product,
-            vendor,
-          }: ''
-            SUBSYSTEMS=="input", ATTRS{id/product}=="${product}", ATTRS{id/vendor}=="${vendor}", SYMLINK+="${name}"
-          '')
-          cfg.disableDevices
+      services.udev.extraRules = (
+        lib.concatStrings (
+          builtins.attrValues (
+            builtins.mapAttrs (name: {
+              product,
+              vendor,
+            }: ''
+              SUBSYSTEMS=="input", ATTRS{id/product}=="${product}", ATTRS{id/vendor}=="${vendor}", SYMLINK+="${name}"
+            '')
+            cfg.disableDevices
+          )
         )
-      )
-    );
+      );
 
-    security.sudo.extraRules = [
-      {
-        users = cfg.allowedUsers;
-        groups = cfg.allowedGroups;
-        commands = [
-          {
-            command = "${./toggle_kb.sh}";
-            options = ["NOPASSWD"];
-          }
-        ];
-      }
-    ];
-  };
+      security.sudo.extraRules = [
+        {
+          users = cfg.allowedUsers;
+          groups = cfg.allowedGroups;
+          commands = [
+            {
+              command = "${cfg.package}/bin/disable-input-devices";
+              options = ["NOPASSWD"];
+            }
+          ];
+        }
+      ];
+    }
+  );
 }
