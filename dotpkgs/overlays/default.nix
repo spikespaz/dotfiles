56dd77b0868b6b7ed2414b84c85249b59bd5d8bc
Jacob Birkett <jacob@birkett.dev>
2022-12-12 06:26:08 -0700

    dotpkgs: overlays: add (broken) fpintd-grosshack

--- dotpkgs/overlays/default.nix
+++ dotpkgs/overlays/default.nix
@@ -17,6 +17,18 @@ lib: (_: prev: {
     });
   });
 
+  # TODO this doesn't work
+  fprintd-grosshack = prev.fprintd.overrideAttrs (old: rec {
+    version = "0.3.0";
+    # doCheck = false;
+    src = prev.fetchFromGitLab {
+      owner = "mishakmak";
+      repo = "pam-fprint-grosshack";
+      rev = "v${version}";
+      sha256 = "sha256-obczZbf/oH4xGaVvp3y3ZyDdYhZnxlCWvL0irgEYIi0=";
+    };
+  });
+
   obs-studio-plugins = (
     with prev.obs-studio-plugins;
       prev.obs-studio-plugins
