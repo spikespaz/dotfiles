791f1cc63668536f9cc4c5a671d8d1aeb144c61c
Jacob Birkett <jacob@birkett.dev>
2023-01-22 03:46:05 -0700

    me: vscode: langs: nix: use nix-ide builtin fmt option

--- users/jacob/programs/vscode/languages/nix.nix
+++ users/jacob/programs/vscode/languages/nix.nix
@@ -5,14 +5,17 @@
 }: {
   programs.vscode.extensions = with pkgs.vscode-marketplace.vscode; [
     jnoortheen.nix-ide
-    kamadorueda.alejandra
+    # kamadorueda.alejandra
   ];
 
   programs.vscode.userSettings = {
     "nix.enableLanguageServer" = true;
     "nix.serverPath" = lib.getExe pkgs.nil;
+    "nix.serverSettings".nil = {
+      formatting.command = [(lib.getExe pkgs.alejandra)];
+    };
     # "nix.formatterPath" = lib.getExe pkgs.alejandra;
-    "alejandra.program" = lib.getExe pkgs.alejandra;
+    # "alejandra.program" = lib.getExe pkgs.alejandra;
     "[nix]" = {
       # appears to be buggy at the moment
       "editor.stickyScroll.enabled" = false;
