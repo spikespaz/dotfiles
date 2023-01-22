5486b7ea81651ae8f2815250d42ede12d3d25c62
Jacob Birkett <jacob@birkett.dev>
2023-01-22 04:12:47 -0700

    systems: tpad: undervolt: minor tidy

--- systems/jacob-thinkpad/undervolt.nix
+++ systems/jacob-thinkpad/undervolt.nix
@@ -10,7 +10,7 @@
     installPhase = ''
       runHook preInstall
 
-      ${lib.getExe pkgs.gnumake}
+      make
       install -Dm555 ./amdctl $out/bin/amdctl
 
       runHook postInstall
@@ -54,12 +54,22 @@ in {
 
       re_vid='s/[A-z ]\+\([0-9]\+\)[A-z ]\+\([0-9]\+\)mV.*/\1/'
 
-      p0_vid=$(${lib.getExe amdctl} -p0 -u${toString (default.p0 + offset.p0)} \
-        | sed "$re_vid")
-      p1_vid=$(${lib.getExe amdctl} -p1 -u${toString (default.p1 + offset.p1)} \
-        | sed "$re_vid")
-      p2_vid=$(${lib.getExe amdctl} -p2 -u${toString (default.p2 + offset.p2)} \
-        | sed "$re_vid")
+      p0_vid="$(
+        ${lib.getExe amdctl} -p0 -u${toString (default.p0 + offset.p0)} \
+        | sed "$re_vid"
+      )"
+      p1_vid="$(
+        ${lib.getExe amdctl} -p1 -u${toString (default.p1 + offset.p1)} \
+        | sed "$re_vid"
+      )"
+      p2_vid="$(
+        ${lib.getExe amdctl} -p2 -u${toString (default.p2 + offset.p2)} \
+        | sed "$re_vid"
+      )"
+
+      echo "P-State 0 VID = $p0_vid"
+      echo "P-State 1 VID = $p1_vid"
+      echo "P-State 2 VID = $p2_vid"
 
       ${lib.getExe amdctl} -p0 -v$p0_vid
       ${lib.getExe amdctl} -p1 -v$p1_vid
