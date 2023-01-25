2baffde5a1b0d12a09c6ea911d9b631f4fa2a5e2
Jacob Birkett <jacob@birkett.dev>
2023-01-25 10:17:38 -0700

    me: wayland: move theme options from profile.nix

--- users/jacob/desktops/wayland/theming.nix
+++ users/jacob/desktops/wayland/theming.nix
@@ -1,9 +1,14 @@
 {
   config,
+  pkgs,
   hmModules,
   ...
 }: {
-  imports = [hmModules.randbg];
+  imports = [
+    hmModules.randbg
+    hmModules.uniform-theme
+    hmModules.kvantum
+  ];
 
   # randomly cycle the wallpaper every hour with a 25% chance
   services.randbg = {
@@ -11,5 +16,57 @@
     interval = 60 * 60;
     chance = 25;
     directory = "${config.home.homeDirectory}/Pictures/Wallpapers";
+  };
+
+  home.uniformTheme = {
+    enable = true;
+    dark = true;
+    cursor = {
+      package = pkgs.quintom-cursor-theme;
+      name = "Quintom_Ink";
+      size = 24;
+    };
+    icons = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+    gtk = {
+      package = pkgs.materia-theme;
+      name = "Materia-dark-compact";
+    };
+    fonts = {
+      default = {
+        package = pkgs.ubuntu_font_family;
+        name = "Ubuntu";
+      };
+      monospace = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Mono";
+      };
+    };
+  };
+
+  programs.kvantum = {
+    enable = true;
+    qt5ct.enable = true;
+    theme.package = pkgs.materia-kde-theme;
+    theme.name = "MateriaDark";
+    theme.overrides = {
+      General = {
+        no_inactiveness = true;
+        translucent_windows = true;
+        reduce_window_opacity = 13;
+        reduce_menu_opacity = 13;
+        drag_from_buttons = false;
+        shadowless_popup = true;
+        popup_blurring = true;
+        menu_blur_radius = 5;
+        tooltip_blur_radius = 5;
+      };
+      Hacks = {
+        transparent_dolphin_view = true;
+        style_vertical_toolbars = true;
+      };
+    };
   };
 }
--- users/jacob/profile.nix
+++ users/jacob/profile.nix
@@ -73,10 +73,6 @@ in {
 
     hmModules.homeage
 
-    ### THEMING MODULES ###
-    hmModules.uniform-theme
-    hmModules.kvantum
-
     ### DEFAULT PROGRAMS ###
     user.mimeapps
 
@@ -172,60 +168,4 @@ in {
     ### DEVICE MANAGEMENT ###
     services.udiskie
   ];
-
-  ############################
-  ### APPEARANCE & THEMING ###
-  ############################
-
-  home.uniformTheme = {
-    enable = true;
-    dark = true;
-    cursor = {
-      package = pkgs.quintom-cursor-theme;
-      name = "Quintom_Ink";
-      size = 24;
-    };
-    icons = {
-      package = pkgs.papirus-icon-theme;
-      name = "Papirus-Dark";
-    };
-    gtk = {
-      package = pkgs.materia-theme;
-      name = "Materia-dark-compact";
-    };
-    fonts = {
-      default = {
-        package = pkgs.ubuntu_font_family;
-        name = "Ubuntu";
-      };
-      monospace = {
-        package = pkgs.dejavu_fonts;
-        name = "DejaVu Sans Mono";
-      };
-    };
-  };
-
-  programs.kvantum = {
-    enable = true;
-    qt5ct.enable = true;
-    theme.package = pkgs.materia-kde-theme;
-    theme.name = "MateriaDark";
-    theme.overrides = {
-      General = {
-        no_inactiveness = true;
-        translucent_windows = true;
-        reduce_window_opacity = 13;
-        reduce_menu_opacity = 13;
-        drag_from_buttons = false;
-        shadowless_popup = true;
-        popup_blurring = true;
-        menu_blur_radius = 5;
-        tooltip_blur_radius = 5;
-      };
-      Hacks = {
-        transparent_dolphin_view = true;
-        style_vertical_toolbars = true;
-      };
-    };
-  };
 }
