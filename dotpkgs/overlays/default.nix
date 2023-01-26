f9c8193a479f359f33a033c092a58a581b72233e
Jacob Birkett <jacob@birkett.dev>
2023-01-26 11:44:47 -0700

    overlays: oraclejdk: hack around requireFile

--- dotpkgs/overlays/default.nix
+++ dotpkgs/overlays/default.nix
@@ -12,20 +12,46 @@ lib: (_: prev: {
     };
   });
 
-  # Fuck you Oracle
+  # Oracle, fuck you. 🖕
   # <https://nadwey.eu.org/java/8/jdk-8u351/>
+  # This override calls `jdk-linux-base.nix` with product information
+  # matching a tarball that we can get from the link above.
+  # In order to hide the "we can't download that for you" message
+  # shown by `pkgs.requireFile` in the nixpkgs source,
+  # we hijack the `requireFile` function passed to `package` below
+  # and if the name matches the product information for this
+  # shady tarball, substitute it with the automatic download.
+  # Note that this does not take into account anything except the basic
+  # default package options. If you need the JCE for example, you're on your own.
+  # Read up on the nixpkgs source to understand this more:
+  # <https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/development/compilers/oraclejdk/jdk-linux-base.nix#L80>
   oraclejdk = let
-    jdk8-linux = import "${prev.path}/pkgs/development/compilers/oraclejdk/jdk-linux-base.nix" {
+    product = {
       productVersion = "8";
       patchVersion = "351";
       sha256.x86_64-linux = "07fw6j38gz0jwxg9qkzsdjzxcnivwq48i9b7pmy7fgk184qcl2gr";
       jceName = "jce_policy-8.zip";
       sha256JCE = "";
     };
+    src = prev.fetchzip {
+      url = "https://nadwey.eu.org/java/8/jdk-8u351/jdk-8u351-linux-x64.tar.gz";
+      sha256 = "sha256-vHqjfVqEzWHaIOeZglzAoIwxTxhmy5AbJVsyoJgSDcg=";
+    };
+    package = import "${prev.path}/pkgs/development/compilers/oraclejdk/jdk-linux-base.nix" product;
+    platformName = builtins.getAttr prev.stdenv.hostPlatform.system {
+      i686-linux = "linux-i586";
+      x86_64-linux = "linux-x64";
+      armv7l-linux = "linux-arm32-vfp-hflt";
+      aarch64-linux = "linux-aarch64";
+    };
   in
-    prev.callPackage jdk8-linux {
+    prev.callPackage package {
       installjdk = true;
       pluginSupport = false;
+      requireFile = args @ {name, ...}:
+        if name == "jdk-${product.productVersion}u${product.patchVersion}-${platformName}.tar.gz"
+        then src
+        else prev.requireFile args;
     };
 
   # TODO this doesn't work
