20330c064eb1421e70a53cdf705bfcd4b3d52e0a
Jacob Birkett <jacob@birkett.dev>
2023-02-03 02:50:26 -0700

    systems: tpad: pia-openvpn: simplify generator

--- systems/jacob-thinkpad/pia-openvpn.nix
+++ systems/jacob-thinkpad/pia-openvpn.nix
@@ -1,43 +1,37 @@
 {
   pkgs,
   lib,
-  flib,
   ...
 }: let
   authUserPass = {
     username = "";
     password = "";
   };
+  updateResolvConf = true;
 
-  pia-openvpn = pkgs.fetchzip {
+  src = pkgs.fetchzip {
     stripRoot = false;
     url = "https://www.privateinternetaccess.com/openvpn/openvpn.zip";
     sha256 = "sha256-ZA8RS6eIjMVQfBt+9hYyhaq8LByy5oJaO9Ed+x8KtW8=";
   };
 
-  configs = lib.pipe (builtins.readDir "${pia-openvpn}") [
-    # Step one: filter dir listing to include only *.ovpn files
-    (lib.filterAttrs (fName: fType: let
-      splitName = flib.rsplit "." fName;
-      fExt = flib.imply splitName splitName.r;
-    in
-      fType == "regular" && flib.imply fExt (fExt == "ovpn")))
-    # Step two:
-    # - remove extension from config name
-    # - read the config file to a string
-    # - create new attrset with other options set, inherit config string
-    (lib.mapAttrs' (fName: _: let
-      name = (flib.rsplit "." fName).l;
-      config = builtins.readFile "${pia-openvpn}/${fName}";
-    in {
-      inherit name;
-      value = {
+  configs = removeAttrs (lib.mapAttrs' (file: type: let
+    match = builtins.match "(.+)\\.ovpn" file;
+    name =
+      if type == "regular" && match != null
+      then builtins.elemAt match 0
+      else "_";
+    value =
+      if name != null
+      then {
+        inherit authUserPass updateResolvConf;
         autoStart = false;
-        updateResolvConf = true;
-        inherit config authUserPass;
-      };
-    }))
-  ];
+        config = builtins.readFile "${src}/${file}";
+      }
+      else null;
+  in {
+    inherit name value;
+  }) (builtins.readDir src.outPath)) ["_"];
 in {
   services.openvpn.servers = configs;
 }
