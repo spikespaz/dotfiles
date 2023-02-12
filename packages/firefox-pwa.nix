ac81fc87ceb4ecb4bef2fa19f57382a2f35d739c
Jacob Birkett <jacob@birkett.dev>
2023-02-12 08:45:42 -0700

    packages: ffpwa: remove needless manual dep hashes

--- packages/firefox-pwa.nix
+++ packages/firefox-pwa.nix
@@ -21,14 +21,7 @@
     inherit version;
 
     src = "${source}/native";
-    cargoLock = {
-      lockFile = "${source}/native/Cargo.lock";
-      outputHashes = {
-        "data-url-0.1.0" = "sha256-rESQz5jjNpVfIuTaRCAV2TLeUs09lOaLZVACsb/3Adg=";
-        "web_app_manifest-0.0.0" = "sha256-CpND9SxPwFmXe6fINrvd/7+HHzESh/O4GMJzaKQIjc8=";
-        "mime-0.4.0-a.0" = "sha256-LjM7LH6rL3moCKxVsA+RUL9lfnvY31IrqHa9pDIAZNE=";
-      };
-    };
+    cargoSha256 = "sha256-nPOyJUyfBVOZgWUOYSgr7HAhGUFlvjtJPNF8gC7YuY8=";
 
     doCheck = false;
 
