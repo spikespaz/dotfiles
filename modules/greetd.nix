8de88d9e4580c339dae381a456518815c96bb278
Jacob Birkett <jacob@birkett.dev>
2023-03-08 12:34:52 -0700

    modules: greetd: new typeSession for entries

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -6,11 +6,39 @@
 }: let
   inherit (lib) types;
   cfg = config.services.greetd;
+
+  typeSession = types.submodule {
+    options = {
+      name = lib.mkOption {
+        type = types.singleLineStr;
+        # default = "";
+        description = lib.mdDoc ''
+          The name of the session to be used in the Desktop Entry.
+        '';
+        example = lib.literalExpression ''TODO'';
+      };
+      comment = lib.mkOption {
+        type = types.string;
+        # default = "";
+        description = lib.mdDoc ''
+          The description to be used in the Desktop Entry.
+        '';
+        example = lib.literalExpression ''TODO'';
+      };
+      script = lib.mkOption {
+        type = types.lines;
+        # default = "";
+        description = lib.mdDoc ''
+          Lines of shell code to start the desktop session.
+        '';
+      };
+    };
+  };
 in {
   options = {
     services.greetd = {
       sessions = lib.mkOption {
-        type = types.attrsOf types.attrs;
+        type = types.attrsOf typeSession;
         default = {};
         description = lib.mdDoc ''TODO'';
         example = lib.literalExpression ''TODO'';
@@ -24,6 +52,7 @@ in {
       };
     };
   };
+
   config = {
     services.greetd.sessionData =
       pkgs.runCommand "generate-sessions" {
