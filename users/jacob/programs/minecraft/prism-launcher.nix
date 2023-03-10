14509a4b3b10b80d126697f955816c53e7354986
Jacob Birkett <jacob@birkett.dev>
2023-03-09 19:48:09 -0700

    me: progs: mc: update graalvm8-ce drv for unstable changes

--- users/jacob/programs/minecraft/prism-launcher.nix
+++ users/jacob/programs/minecraft/prism-launcher.nix
@@ -1,48 +1,10 @@
 {
   pkgs,
   nixpkgs,
+  nixpkgs-stable,
   ...
 }: let
-  # there are a few open issues but this seems simplest
-  # <https://sourcegraph.com/github.com/NixOS/nixpkgs/-/blob/pkgs/development/compilers/graalvm/community-edition/mkGraal.nix>
-  mkGraal = opts:
-    pkgs.callPackage (import "${nixpkgs}/pkgs/development/compilers/graalvm/community-edition/mkGraal.nix" opts) {
-      Foundation = null;
-    };
-
-  # TODO clean up the nixpkgs source code,
-  # only run what is necessary and remove the stupidity here
-  graalvm8-ce =
-    (mkGraal {
-      config = {
-        x86_64-linux = {
-          products = [
-            "graalvm-ce"
-            # "native-image-installable-svm"
-          ];
-          arch = "linux-amd64";
-        };
-      };
-      defaultVersion = "21.3.1";
-      javaVersion = "8";
-      sourcesPath = ./graalvm8-ce-sources.json;
-    })
-    .overrideAttrs (old: {
-      # why do I have to do this? the native image is disabled
-      preInstall = ''
-        mkdir -p $out/lib/svm/clibraries/linux-amd64
-      '';
-
-      # nixpkgs needs to be audited badly,
-      # we are missing the pre- and post- hooks
-      installPhase = ''
-        runHook preInstall
-        ${old.installPhase}
-        runHook postInstall
-      '';
-    });
-
-  graalvm8-ce-jre = "${graalvm8-ce}/jre";
+  inherit (pkgs.callPackage ./graalvm.nix {}) graalvm8-ce-jre;
 
   javaPackages = [
     # Java 8
