344bfff002384937550c10f9b3fd94499f71abef
Jacob Birkett <spikespaz@outlook.com>
2023-04-21 23:49:13 -0700

    inputs: update slight for new gammastep module

--- users/jacob/desktops/wayland/gammastep.nix
+++ users/jacob/desktops/wayland/gammastep.nix
@@ -1,14 +1,14 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{hmModules, ...}: {
+  imports = [
+    hmModules.gammastep-hook
+  ];
+
   services.gammastep = {
     enable = true;
     tray = true;
     dawnTime = "6:30-8:00"; # 6:30 AM to 8:00 AM
-    duskTime = "20:30-22:00"; # 8:30 PM to 10:00 PM
+    # duskTime = "20:30-22:00"; # 8:30 PM to 10:00 PM
+    duskTime = "18:30-21:00"; # 6:30 PM to 9:00 PM
     provider = "geoclue2";
     temperature.day = 6500;
     temperature.night = 3700;
@@ -16,82 +16,16 @@
       fade = true;
       adjustment-method = "wayland";
     };
+
+    slight.brightnessHook = {
+      enable = true;
+      brightness.day = 90;
+      brightness.transition = 60;
+      brightness.night = 30;
+      interpDur.dayFromTransition = "10s";
+      interpDur.nightFromTransition = "10s";
+      interpDur.transitionFromDay = "20s";
+      interpDur.transitionFromNight = "30s";
+    };
   };
-
-  # TODO integrate this into slight
-  xdg.configFile."gammastep/hooks/brightness.sh" = let
-    slight = lib.getExe pkgs.slight;
-    brightness.day = 90;
-    brightness.transition = 60;
-    brightness.night = 30;
-    # to day from transition
-    duration.day = "5s";
-    # to transition from day
-    duration.transition.day = "10s";
-    # to transition from night
-    duration.transition.night = "10s";
-    # to night from transition
-    duration.night = "20s";
-  in {
-    executable = true;
-    source =
-      (pkgs.writeShellScript "gammastep-brightness" ''
-        set -eu
-
-        exec >> /tmp/redshift-hooks.log 2>&1
-
-        if [ "$1" = 'period-changed' ]; then
-          case "$3" in
-            daytime)
-              target="${toString brightness.day}%"
-              case "$2" in
-                  transition)
-                    ${slight} set -I "$target" -t ${duration.day}
-                    ;;
-                  night|none)
-                    ${slight} set "$target"
-                    ;;
-                  *)
-                    echo "unrecognized: $2"
-                    ;;
-              esac
-              ;;
-            transition)
-              target="${toString brightness.transition}%"
-              case "$2" in
-                  daytime)
-                    ${slight} set -D "$target" -t ${duration.transition.day}
-                    ;;
-                  night)
-                    ${slight} set -I "$target" -t ${duration.transition.night}
-                    ;;
-                  none)
-                    ${slight} set "$target"
-                    ;;
-                  *)
-                    echo "unrecognized: $2"
-                    ;;
-              esac
-              ;;
-            night)
-              target="${toString brightness.night}%"
-              case "$2" in
-                transition)
-                  ${slight} set -D "$target" -t ${duration.night}
-                  ;;
-                daytime|none)
-                  ${slight} set "$target"
-                  ;;
-                *)
-                  echo "unrecognized: $2"
-                  ;;
-              esac
-              ;;
-          esac
-        fi
-      '')
-      .outPath;
-  };
-
-  # <https://wiki.archlinux.org/title/redshift#Use_real_screen_brightness>
 }
