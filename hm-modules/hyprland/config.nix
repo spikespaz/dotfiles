c9e8ea2bb236b56d8f25f1cc0279620e6a3cac42
Jacob Birkett <spikespaz@outlook.com>
2023-04-21 23:49:37 -0700

    hm-modules: hl: config: reimplement atomic config

--- hm-modules/hyprland/config.nix
+++ hm-modules/hyprland/config.nix
@@ -5,15 +5,67 @@
   ...
 }: let
   inherit (lib) types;
-  cfg = config.wayland.windowManager.hyprland.alt;
+  cfg = config.wayland.windowManager.hyprland // config.wayland.windowManager.hyprland.alt;
+  cfgPath = "config.wayland.windowManager.hyprland.alt";
 in {
   options = {
     wayland.windowManager.hyprland.alt = {
-      enableConfig = lib.mkEnableOption "Nix-generated Hyprland config";
+      enableConfig = lib.mkEnableOption (lib.mdDoc ''
+        Enable writing the Hyprland configuration file.
+
+        `~/.config/hypr/hyprland.conf`
+      '');
+
+      # This replaces `wayland.windowManager.hyprland.extraConfig`.
+      configLines = lib.mkOption {
+        type = types.lines;
+        description = lib.mdDoc ''
+          Lines of the hyprland config to write.
+        '';
+      };
+
+      reloadConfig = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = lib.mdDoc ''
+          If enabled, automatically tell Hyprland to reload configuration
+          after activating a new Home Manager generation.
+
+          Note, this option is different from
+          `${cfgPath}.config.misc.disable_autoreload`,
+          which disables Hyprland's filesystem watch.
+        '';
+      };
+
+      ### CONFIG ###
+
+      extraInitConfig = lib.mkOption {
+        type = types.nullOr types.lines;
+        default = null;
+        description = lib.mdDoc ''
+          Extra configuration to be prepended to the top of
+          `~/.config/hypr/hyprland.conf` (after module's generated init).
+        '';
+        # example = lib.literalExpression "";
+      };
+
+      extraConfig = lib.mkOption {
+        type = types.nullOr types.lines;
+        default = null;
+        description = lib.mdDoc ''
+          Extra configuration lines to append to the bottom of
+          `~/.config/hypr/hyprland.conf`.
+        '';
+      };
+
       config = lib.mkOption {
         type = types.attrs;
         default = {};
-        description = lib.mdDoc "Hyprland config attributes";
+        description = lib.mdDoc ''
+          Hyprland config attributes.
+          These will be serialized to lines of text,
+          included in `configLines`.
+        '';
       };
     };
   };
@@ -48,7 +100,46 @@ in {
       then lib.concatMapStringsSep " " valueToString value
       else abort (lib.traceSeqN 2 value "Invalid value, cannot convert '${builtins.typeOf value}' to Hyprland config string value");
   in
-    lib.mkIf cfg.enableConfig {
-      xdg.configFile."hypr/hyprland.conf".text = lib.traceVal (configSection 0 cfg.config);
-    };
+    lib.mkMerge [
+      (lib.mkIf (cfg.extraInitConfig != null) {
+        wayland.windowManager.hyprland.alt.configLines = lib.mkOrder 0 cfg.extraInitConfig;
+      })
+      (lib.mkIf cfg.systemdIntegration {
+        wayland.windowManager.hyprland.alt.configLines = lib.mkOrder 50 ''
+          exec-once=${pkgs.dbus}/bin/dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY HYPRLAND_INSTANCE_SIGNATURE XDG_CURRENT_DESKTOP
+          exec-once=systemctl --user start hyprland-session.target
+        '';
+      })
+      (lib.mkIf (cfg.config != null) {
+        wayland.windowManager.hyprland.alt.configLines = lib.mkOrder 350 (configSection 0 cfg.config);
+      })
+      (lib.mkIf (cfg.extraConfig != null) {
+        wayland.windowManager.hyprland.alt.configLines = lib.mkOrder 900 cfg.extraConfig;
+      })
+      (lib.mkIf cfg.enableConfig {
+        # We do not want the original `onChange`
+        wayland.windowManager.hyprland.disableAutoreload = true;
+
+        # Create the config file with content from `configLines`.
+        # This replaces `wayland.windowManager.hyprland.extraConfig`.
+        xdg.configFile."hypr/hyprland.conf" = {
+          text = cfg.configLines;
+          onChange = lib.mkIf (cfg.reloadConfig) ''
+            (
+              shopt -s nullglob
+              for socket in /tmp/hypr/_*/.socket.sock; do
+                response="$(
+                  printf 'reload config-only' \
+                    | ${pkgs.netcat}/bin/nc -U $socket 2>/dev/null || true
+                )"
+                if [[ "$response" == 'ok' ]]; then
+                  instance="$(egrep -o '_[0-9]+' <<< $socket)"
+                  echo "Reloading Hyprland instance $instance"
+                fi
+              done
+            )
+          '';
+        };
+      })
+    ];
 }
