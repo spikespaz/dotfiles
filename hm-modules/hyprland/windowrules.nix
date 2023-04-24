eb9712ece21ae8e9759e717aa965a4b02499e502
Jacob Birkett <spikespaz@outlook.com>
2023-04-24 13:04:36 -0700

    hm-mods: hl: windowrules: document functions

--- hm-modules/hyprland/windowrules.nix
+++ hm-modules/hyprland/windowrules.nix
@@ -4,7 +4,7 @@
   ...
 }: let
   inherit (lib) types;
-  cfg = config.wayland.windowManager.hyprland.windowRules;
+  cfg = config.wayland.windowManager.hyprland;
 in {
   options = {
     wayland.windowManager.hyprland.windowRules = lib.mkOption {
@@ -13,14 +13,24 @@ in {
           class = lib.mkOption {
             type = types.nullOr (types.listOf types.singleLineStr);
             default = null;
+            description = lib.mdDoc ''
+              A list of class names to match against.
+            '';
           };
           title = lib.mkOption {
             type = types.nullOr (types.listOf types.singleLineStr);
             default = null;
+            description = lib.mdDoc ''
+              A list of window titles to match against.
+            '';
           };
           rules = lib.mkOption {
             type = types.nullOr (types.listOf types.singleLineStr);
             default = null;
+            description = lib.mdDoc ''
+              A list of window rules to apply to a window matching
+              both the class names and titles given.
+            '';
           };
         };
       });
@@ -88,46 +98,59 @@ in {
   };
 
   config = let
-    expandRule = {
-      rules,
-      class ? null,
-      title ? null,
-    }: (
+    # Given a second-form window rule (one with a `rules` list)
+    # convert it into a list of first-form window rules
+    # (one with a single string value for the `rule` attr).
+    expandWindowRuleToList = rule2:
       map (rule: {
-        inherit rule class title;
+        inherit (rule2) class title;
+        inherit rule;
       })
-      rules
-    );
-    compileRule = {
-      rule,
-      class,
-      title,
-    }: {
-      inherit rule;
-      class =
-        lib.mapNullable
-        (x: "class:^(${lib.concatStringsSep "|" x})$")
-        class;
-      title =
-        lib.mapNullable
-        (x: "title:^(${lib.concatStringsSep "|" x})$")
-        title;
-    };
-    stringifyRule = {
-      rule,
-      class,
-      title,
-    }: "${lib.concatStringsSep ", " (
-      [rule]
-      ++ (lib.optional (class != null) class)
-      ++ (lib.optional (title != null) title)
-    )}";
+      rule2.rules;
+    # Given a window rule in second- or first-form
+    # (with `rule` or `rules` attr respectively),
+    # turn the `class` and `title` list into regex patterns
+    # matching any string in each list.
+    compileWindowRulePatterns = rule:
+      rule
+      // {
+        class =
+          lib.mapNullable
+          (x: "class:^(${lib.concatStringsSep "|" x})$")
+          rule.class;
+        title =
+          lib.mapNullable
+          (x: "title:^(${lib.concatStringsSep "|" x})$")
+          rule.title;
+      };
+    # Convert a rule (first-form) into a valid string value for
+    # a Hyprland config variable.
+    windowRuleToString = rule:
+      lib.concatStringsSep ", " (
+        [rule.rule]
+        ++ (lib.optional (rule.class != null) rule.class)
+        ++ (lib.optional (rule.title != null) rule.title)
+      );
+
+    _traceMsgVal = msg: val: "${msg}\n${lib.generators.toPretty {multiline = true;} val}";
+
+    traceM = m: v: builtins.trace (_traceMsgVal m v);
+    traceValM = m: v: builtins.trace (_traceMsgVal m v) v;
   in {
-    wayland.windowManager.hyprland.config.windowrulev2 = lib.pipe cfg [
-      (map expandRule)
+    wayland.windowManager.hyprland.config.windowrulev2 = lib.pipe cfg.windowRules [
+      # right now we have window rules in second-form,
+      # each "rule" has a `rules` attr which is a list of string-rules.
+      #
+      # the first step is to turn `class` and `title` into regex strings
+      # for each second-form rule.
+      (map compileWindowRulePatterns)
+      # then expand each into a list of rules in first-form,
+      # with `rule` instead of `rules`.
+      (map expandWindowRuleToList)
+      # combine the intermediate lists to a single list of first-form rules.
       lib.concatLists
-      (map compileRule)
-      (map stringifyRule)
+      # map each to a Hyprland config variable value string.
+      (map windowRuleToString)
     ];
   };
 }
