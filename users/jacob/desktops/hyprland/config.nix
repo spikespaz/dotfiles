69910e915fc191c565cb5ed8f5dde784970c8ef4
Jacob Birkett <spikespaz@outlook.com>
2023-06-01 22:29:32 -0700

    me: hl: configure group bar titles

--- users/jacob/desktops/hyprland/config.nix
+++ users/jacob/desktops/hyprland/config.nix
@@ -21,7 +21,6 @@
       blur_size = 2;
       blur_passes = 3;
       blur_ignore_opacity = true;
-      blur_new_optimizations = true;
       shadow_range = 10;
       shadow_render_power = 2;
       active_shadow_color = "rgba(0F0F0FE6)";
@@ -36,7 +35,11 @@
       follow_mouse = LOOSE;
       float_switch_override_focus = DISABLED;
 
-      touchpad = { tap_to_click = false; };
+      touchpad = {
+        # tap_to_click = true;
+        tap_and_drag = true;
+        # drag_lock = true;
+      };
     };
 
     # <https://wiki.hyprland.org/Configuring/Variables/#binds>
@@ -60,6 +63,13 @@
       variable_framerate = true;
       variable_refresh = FULLSCREEN_ONLY;
       disable_autoreload = true; # false # nix takes care of that
+
+      # these features are not polished yet
+      # <https://github.com/hyprwm/Hyprland/issues/2415>
+      render_titles_in_groupbar = true;
+      groupbar_titles_font_size = 9;
+      groupbar_gradients = false;
+      groupbar_text_color = "rgba(FBF1C7FF)"; # fg0
     };
 
     # <https://wiki.hyprland.org/Configuring/Dwindle-Layout/>
