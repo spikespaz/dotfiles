c3dfe375fb5632a63649b294658a0fe6f0addd05
Jacob Birkett <spikespaz@outlook.com>
2023-06-23 01:20:07 -0700

    packages: ja-netfilter: fix for when programName == null

--- packages/ja-netfilter/default.nix
+++ packages/ja-netfilter/default.nix
@@ -7,16 +7,16 @@ let
   pluginPackages =
     lib.optionals (enabledPlugins != null) (map callPlugin enabledPlugins);
   configFiles = lib.optionals (pluginConfigs != null) (lib.mapAttrsToList
-    (name: value:
-      writeTextDir "share/ja-netfilter/config-${programName}/${name}.conf"
-      value) pluginConfigs);
+    (name: value: writeTextDir "share/ja-netfilter/config/${name}.conf" value)
+    pluginConfigs);
 in symlinkJoin {
   name = if programName == null then
     "ja-netfilter"
   else
     "ja-netfilter-${programName}";
   paths = [ ja-netfilter ] ++ pluginPackages ++ configFiles;
-  postBuild = ''
+  postBuild = lib.optionalString (programName != null) ''
     mv $out/share/ja-netfilter/plugins $out/share/ja-netfilter/plugins-${programName}
+    mv $out/share/ja-netfilter/config $out/share/ja-netfilter/config-${programName}
   '';
 }
