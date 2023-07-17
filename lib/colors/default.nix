8433d9b54b8faabc73c0a6d3f7e2cf113fd45c0d
Jacob Birkett <spikespaz@outlook.com>
2023-07-17 06:36:33 -0700

    lib: colors: add functions to convert to hex

--- lib/colors/default.nix
+++ lib/colors/default.nix
@@ -1,9 +1,19 @@
 { lib }:
 let
-  rgbAttrsToHexStr = { r, g, b }:
-    (lib.intToHex r) + (lib.intToHex g) + (lib.intToHex b);
+  # Produces a hexadecimal RRGGBBAA color from an attributes.
+  # Each channel value is expected to be between 0-255.
+  # Alpha attribute `a` is not required and may be undefined or null.
+  hexRGBA = { r, g, b, a ? null }:
+    lib.concatStrings (map (c: lib.lpadString "0" 2 (lib.intToHex c))
+      ([ r g b ] ++ lib.optional (a != null) a));
+
+  # Like `hexRGBA` but takes alpha as float in a separate argument.
+  hexRGBA' = rgb: a:
+    let a' = builtins.floor (a * 255);
+    in hexRGBA (rgb // { a = a'; });
 
   gruvbox = import ./palettes/gruvbox.nix { inherit lib; };
 in {
+  inherit hexRGBA hexRGBA';
   palettes = { inherit gruvbox; };
 }
