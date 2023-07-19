feb1662470589e9b0ab1e97a600f4d8b212cbb7c
Jacob Birkett <spikespaz@outlook.com>
2023-07-19 01:43:03 -0700

    hm-mods: swayidle: impl timeouts for bat/ac

--- hm-modules/swayidle.nix
+++ hm-modules/swayidle.nix
@@ -34,6 +34,14 @@ let
       };
     };
   };
+
+  typeRootPath = lib.mkOptionType {
+    name = "rootPath";
+    description = "path starting from root";
+    descriptionClass = "noun";
+    check = x: builtins.isString x && builtins.substring 0 1 x == "/";
+    merge = lib.mergeEqualOption;
+  };
 in {
   options = {
     services.swayidle.alt = {
@@ -109,9 +117,17 @@ in {
         default = { };
         example = lib.literalExpression "";
         description = lib.mdDoc ''
+          > If you are using a laptop, you may prefer to use
+          > {option}`batteryTimeouts` and {option}`pluggedInTimeouts`.
+          >
+          > Timeout scripts defined in this option will be run
+          > regardless of state, and have no lock file.
+
           An attribute set of timeout definitions.
           The attribute name is the used internally to name
-          shell scripts written to the store.
+          shell scripts written to the store, and lock files
+          if the scripts are state-dependant (in the case of
+          {option}`batteryTimeouts` and {option}`pluggedInTimeouts`).
 
           A timeout definition is an attribute set
           of keys `timeout`, `script`, and `resumeScript`.
@@ -122,10 +138,68 @@ in {
           and the session is no longer considered to be idle.
         '';
       };
+
+      batteryDevice = lib.mkOption {
+        type = typeRootPath;
+        default = "/sys/class/power_supply/BAT0";
+        example = lib.literalExpression "/sys/class/power_supply/BAT1";
+        description = lib.mdDoc ''
+          The path to the battery device to check.
+          Must be a directory with a `status` file.
+
+          This path is expected to match the Linux kernel's
+          [sysfs-class-power](https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-power)
+          specification.
+
+          Timeouts from {option}`pluggedInTimeouts` will be run
+          if the contents of the `status` file equals either
+          `Charging` and `Not charging`, otherwise timeouts from
+          {options}`batteryTimeouts` will be run instead.
+        '';
+      };
+
+      batteryTimeouts = lib.mkOption {
+        type = types.attrsOf (types.submodule typeTimeout);
+        default = { };
+        example = lib.literalExpression "";
+        description = lib.mdDoc ''
+          > This option is only relevant for laptop users.
+          > For devices powered directly from AC,
+          > use {option}`timeouts` instead.
+
+          Timeouts to run only when on battery power.
+          See {option}`timeouts` for details.
+
+          This is determined by checking if the `status` file in
+          {option}`batteryDevice` path contains anything **other than**
+          `Charging` or `Not charging`.
+        '';
+      };
+
+      pluggedInTimeouts = lib.mkOption {
+        type = types.attrsOf (types.submodule typeTimeout);
+        default = { };
+        example = lib.literalExpression "";
+        description = lib.mdDoc ''
+          > This option is only relevant for laptop users.
+          > For devices powered directly from AC,
+          > use {option}`timeouts` instead.
+
+          Timeouts to run only when plugged in with an AC adapter.
+          See {option}`timeouts` for details.
+
+          This is determined by checking if the `status` file in
+          {option}`batteryDevice` path contains **either**
+          `Charging` or `Not charging`.
+        '';
+      };
     };
   };
 
   config = lib.mkIf cfg.enable (let
+    # make sure this is double quoted everywhere it is used
+    lockFileDir = "/var/run/user/$(id -u)/swayidle";
+
     mkEventArgs = name: script:
       let
         eventNames = {
@@ -146,10 +220,57 @@ in {
         (pkgs.writeShellScript "swayidle-${scriptName}-resume"
           resumeScript).outPath
       ];
+
+    wrapCondTimeout = suffix: condition:
+      { scriptName, timeout, script, resumeScript }:
+      let
+        scriptName' = "${scriptName}-${suffix}";
+        # FIXME assumes there are no slashes in the name
+        lockPath = "${lockFileDir}/${scriptName'}.lock";
+      in {
+        scriptName = scriptName';
+        inherit timeout;
+        script = ''
+          if ${condition}; then
+            ${script}
+            ${
+              lib.optionalString (resumeScript != null) ''
+                mkdir -p "${lockFileDir}"
+                touch "${lockPath}"
+              ''
+            }
+          fi
+        '';
+        resumeScript = if resumeScript == null then
+          null
+        else ''
+          if [ -f "${lockPath}" ]; then
+            ${resumeScript}
+            rm "${lockPath}"
+          fi
+        '';
+      };
+
+    # maybe make this configurable?
+    batStatusCond = states:
+      "${lib.getExe pkgs.gnugrep} -q -x -F ${
+        lib.concatMapStrings (s: " -e '${s}'") states
+      } ${cfg.batteryDevice}/status";
+    pluggedInCond = batStatusCond [ "Charging" "Not charging" ];
+
     args = lib.flatten [
       (map lib.escapeShellArg cfg.extraArgs)
+
       (lib.mapAttrsToList mkEventArgs cfg.events)
+
       (map mkTimeoutArgs (builtins.attrValues cfg.timeouts))
+
+      (map (x: mkTimeoutArgs (wrapCondTimeout "battery" "! ${pluggedInCond}" x))
+        (builtins.attrValues cfg.batteryTimeouts))
+
+      (map (x: mkTimeoutArgs (wrapCondTimeout "pluggedIn" "${pluggedInCond}" x))
+        (builtins.attrValues cfg.pluggedInTimeouts))
+
       (lib.optionals (cfg.idleHint != null) [
         "idlehint"
         (toString cfg.idleHint)
