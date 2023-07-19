5913918a8924045274d8cdfb7103f99272a26655
Jacob Birkett <spikespaz@outlook.com>
2023-07-19 01:45:13 -0700

    me: wl: timeouts: update cfg for module ac/bat integration

--- users/jacob/desktops/wayland/timeouts.nix
+++ users/jacob/desktops/wayland/timeouts.nix
@@ -1,7 +1,5 @@
 { self, config, lib, pkgs, ... }:
 let
-  # <https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-power>
-  batteryDevice = "/sys/class/power_supply/BAT0";
   # <https://docs.kernel.org/leds/leds-class.html>
   kbdLightDevice = "/sys/class/leds/tpacpi::kbd_backlight";
 
@@ -47,18 +45,12 @@ in {
   # it reacts to events (such as timeouts) and runs commands
   # <https://github.com/swaywm/swayidle/blob/master/swayidle.1.scd>
   services.swayidle.alt = let
-    grep = lib.getExe pkgs.gnugrep;
     slight = lib.getExe pkgs.slight;
     hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
     swaylock = lib.getExe config.programs.swaylock.package;
 
-    batStatus = states:
-      "${grep} -q -x -F ${
-        lib.concatMapStrings (s: " -e '${s}'") states
-      } ${batteryDevice}/status";
-    pluggedInAC = batStatus [ "Charging" "Not charging" ];
-
     screenDimEnter = { target, duration, lockName }: ''
+      set -eu
       brightness="$(${slight} get -p)"
       brightness="''${brightness/\%/}"
       if [[ "$brightness" -gt ${toString target} ]]; then
@@ -66,27 +58,34 @@ in {
         ${slight} set -D ${toString target} -t ${duration} &
         printf '%s' "$!" > /tmp/${lockName}.pid
       fi
+      set +eu
     '';
     screenDimLeave = { duration, lockName }: ''
+      set -eu
       brightness="$(cat /tmp/${lockName})"
       kill "$(cat /tmp/${lockName}.pid)" || true
       ${slight} set -I "$brightness%" -t ${duration}
       rm -f /tmp/${lockName}{,.pid}
+      set +eu
     '';
 
     # no real PID lock file (just saved brightness) because
     # this is basically instant with no duration.
     kbdLightOffEnter = { device, target, lockName }: ''
+      set -eu
       brightness="$(${slight} -D ${kbdLightDevice} get)"
       if [[ "$brightness" -gt ${toString target} ]]; then
         printf '%s' "$brightness" > /tmp/${lockName}
         ${slight} -D ${device} set -D ${toString target}
       fi
+      set +eu
     '';
     kbdLightOffLeave = { device, lockName }: ''
+      set -eu
       brightness="$(cat /tmp/${lockName})"
       ${slight} -D ${device} set -I "$brightness"
       rm -f /tmp/${lockName}{,.pid}
+      set +eu
     '';
   in {
     enable = true;
@@ -106,173 +105,97 @@ in {
       '';
     };
 
-    timeouts = {
-      screenDimBAT = let lockName = ".screen_dim_brightness_bat";
+    batteryTimeouts = {
+      screenDim = let lockName = ".screen_dim_brightness_bat";
       in {
         timeout = screenDimTimeoutBAT;
-        script = ''
-          set -eu
-          if ! ${pluggedInAC}; then
-            ${
-              screenDimEnter {
-                target = screenDimTargetBAT;
-                duration = screenDimEnterDuration;
-                inherit lockName;
-              }
-            }
-          fi
-        '';
-        resumeScript = ''
-          set -eu
-          if [[ -f /tmp/${lockName} ]]; then
-            ${
-              screenDimLeave {
-                duration = screenDimLeaveDuration;
-                inherit lockName;
-              }
-            }
-          fi
-        '';
+        script = screenDimEnter {
+          target = screenDimTargetBAT;
+          duration = screenDimEnterDuration;
+          inherit lockName;
+        };
+        resumeScript = screenDimLeave {
+          duration = screenDimLeaveDuration;
+          inherit lockName;
+        };
       };
 
-      screenDimAC = let lockName = ".screen_dim_brightness_ac";
-      in {
-        timeout = screenDimTimeoutAC;
-        script = ''
-          set -eu
-          if ${pluggedInAC}; then
-            ${
-              screenDimEnter {
-                target = screenDimTargetAC;
-                duration = screenDimEnterDuration;
-                inherit lockName;
-              }
-            }
-          fi
-        '';
-        resumeScript = ''
-          set -eu
-          if [[ -f /tmp/${lockName} ]]; then
-            ${
-              screenDimLeave {
-                duration = screenDimLeaveDuration;
-                inherit lockName;
-              }
-            }
-          fi
-        '';
-      };
-
-      autoLockBAT = {
+      autoLock = {
         timeout = autoLockTimeoutBAT;
         script = ''
-          if ! ${pluggedInAC}; then
-            ${swaylock} -f --grace ${toString autoLockGrace}
-          fi
+          ${swaylock} -f --grace ${toString autoLockGrace}
         '';
       };
 
-      autoLockAC = {
-        timeout = autoLockTimeoutAC;
-        script = ''
-          if ${pluggedInAC}; then
-            ${swaylock} -f --grace ${toString autoLockGrace}
-          fi
-        '';
-      };
-
-      screenOffBAT = let lockName = ".timeout_screen_off_bat";
-      in {
+      screenOff = {
         timeout = screenOffTimeoutBAT;
         script = ''
-          set -eu
-          if ! ${pluggedInAC}; then
-            ${hyprctl} dispatch dpms off
-            touch /tmp/${lockName}
-          fi
+          ${hyprctl} dispatch dpms off
         '';
         resumeScript = ''
-          set -eu
-          if [[ -f /tmp/${lockName} ]]; then
-            ${hyprctl} dispatch dpms on
-            rm /tmp/${lockName}
-          fi
+          ${hyprctl} dispatch dpms on
         '';
       };
 
-      screenOffAC = let lockName = ".timeout_screen_off_ac";
-      in {
-        timeout = screenOffTimeoutAC;
-        script = ''
-          set -eu
-          if ${pluggedInAC}; then
-            ${hyprctl} dispatch dpms off
-            touch /tmp/${lockName}
-          fi
-        '';
-        resumeScript = ''
-          set -eu
-          if [[ -f /tmp/${lockName} ]]; then
-            ${hyprctl} dispatch dpms on
-            rm /tmp/${lockName}
-          fi
-        '';
-      };
-
-      kbdLightOffBAT = let lockName = ".timeout_kbd_light_off_bat";
+      kbdLightOff = let lockName = ".timeout_kbd_light_off_bat";
       in {
         timeout = kbdLightOffTimeoutBAT;
+        script = kbdLightOffEnter {
+          device = kbdLightDevice;
+          target = kbdLightOffValue;
+          inherit lockName;
+        };
+        resumeScript = kbdLightOffLeave {
+          device = kbdLightDevice;
+          inherit lockName;
+        };
+      };
+    };
+
+    pluggedInTimeouts = {
+      screenDim = let lockName = ".screen_dim_brightness_ac";
+      in {
+        timeout = screenDimTimeoutAC;
+        script = screenDimEnter {
+          target = screenDimTargetAC;
+          duration = screenDimEnterDuration;
+          inherit lockName;
+        };
+        resumeScript = screenDimLeave {
+          duration = screenDimLeaveDuration;
+          inherit lockName;
+        };
+      };
+
+      autoLock = {
+        timeout = autoLockTimeoutAC;
         script = ''
-          set -eu
-          if ! ${pluggedInAC}; then
-            ${
-              kbdLightOffEnter {
-                device = kbdLightDevice;
-                target = kbdLightOffValue;
-                inherit lockName;
-              }
-            }
-          fi
-        '';
-        resumeScript = ''
-          set -eu
-            if [[ -f /tmp/${lockName} ]]; then
-              ${
-                kbdLightOffLeave {
-                  device = kbdLightDevice;
-                  inherit lockName;
-                }
-              }
-            fi
+          ${swaylock} -f --grace ${toString autoLockGrace}
         '';
       };
 
-      kbdLightOffAC = let lockName = ".timeout_kbd_light_off_ac";
-      in {
-        timeout = kbdLightOffTimeoutAC;
+      screenOff = {
+        timeout = screenOffTimeoutAC;
         script = ''
-          set -eu
-          if ${pluggedInAC}; then
-            ${
-              kbdLightOffEnter {
-                device = kbdLightDevice;
-                target = kbdLightOffValue;
-                inherit lockName;
-              }
-            }
-          fi
+          ${hyprctl} dispatch dpms off
         '';
         resumeScript = ''
-          set -eu
-          if [[ -f /tmp/${lockName} ]]; then
-            ${
-              kbdLightOffLeave {
-                device = kbdLightDevice;
-                inherit lockName;
-              }
-            }
-          fi
+          ${hyprctl} dispatch dpms on
         '';
+      };
+
+      kbdLightOff = let lockName = ".timeout_kbd_light_off_ac";
+      in {
+        timeout = kbdLightOffTimeoutAC;
+        script = kbdLightOffEnter {
+          device = kbdLightDevice;
+          target = kbdLightOffValue;
+          inherit lockName;
+        };
+        resumeScript = kbdLightOffLeave {
+          device = kbdLightDevice;
+          inherit lockName;
+        };
       };
     };
   };
