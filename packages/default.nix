95674a36c2c9aad736fd4ad90e5433fee11e275d
Jacob Birkett <spikespaz@outlook.com>
2023-07-18 18:42:40 -0700

    packages: default: remove attrs for disabled drvs

--- packages/default.nix
+++ packages/default.nix
@@ -1,15 +1,18 @@
 final: prev: {
+  # Individual packages
+
   ja-netfilter = final.callPackage ./ja-netfilter { };
   prtsc = final.callPackage ./prtsc { };
   ttf-ms-win11 = final.callPackage ./ttf-ms-win11 { };
   fastfetch = final.callPackage ./fastfetch.nix { };
   fork-awesome = final.callPackage ./fork-awesome.nix { };
-  firefox-pwa = final.callPackage ./firefox-pwa.nix { };
   idlehack = final.callPackage ./idlehack.nix { };
-  mcpelauncher = final.qt6.callPackage ./mcpelauncher.nix { };
-  mcpelauncher-qt5 = final.libsForQt5.callPackage ./mcpelauncher.nix { };
   plymouth-themes = final.callPackage ./plymouth-themes.nix { };
-  rofi-themes = final.callPackage ./rofi-themes.nix { };
+  proton-ge-custom = final.callPackage ./proton-ge-custom.nix { };
+  qt6ct = final.qt6.callPackage ./qt6ct.nix { };
+  nerdfonts-symbols-only = final.callPackage ./nerdfonts.nix { };
+
+  # Package sets
 
   obs-studio-plugins = prev.obs-studio-plugins // {
     advanced-scene-switcher =
@@ -18,9 +21,4 @@ final: prev: {
     advanced-scene-switcher-qt5 = final.libsForQt5.callPackage
       ./obs-studio-plugins/advanced-scene-switcher.nix { };
   };
-
-  kvantum-qt6 = final.callPackage ./kvantum-qt6.nix { };
-  proton-ge-custom = final.callPackage ./proton-ge-custom.nix { };
-  qt6ct = final.qt6.callPackage ./qt6ct.nix { };
-  nerdfonts-symbols-only = final.callPackage ./nerdfonts.nix { };
 }
