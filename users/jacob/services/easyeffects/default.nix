748fa9a0973145ebf034062442bba0a87f77cf9b
Jacob Birkett <spikespaz@outlook.com>
2023-07-28 01:12:40 -0700

    me: services: easyeffects: add autoload profiles for Qudelix 5K

--- users/jacob/services/easyeffects/default.nix
+++ users/jacob/services/easyeffects/default.nix
@@ -12,6 +12,12 @@ in {
     };
   };
 
+  # All of this is really dumb.
+  # There should be a default preset that is used when no other sink
+  # matches an autoload configuration.
+  # <https://github.com/wwmm/easyeffects/issues/1359>
+  # Can I just say, EasyEffects is shitty software.
+
   xdg.configFile."easyeffects/output/laptop_unsuck.json".text =
     builtins.toJSON laptop-unsuck;
 
@@ -30,6 +36,31 @@ in {
       device-description =
         "Family 17h/19h HD Audio Controller Speaker + Headphones";
       device-profile = "[Out] Headphones";
+      preset-name = "empty";
+    };
+
+  xdg.configFile."easyeffects/autoload/output/bluez_output.98_8E_79_00_89_6C.1:headphone-output.json".text =
+    builtins.toJSON {
+      device = "bluez_output.98_8E_79_00_89_6C.1";
+      device-description = "Qudelix-5K";
+      device-profile = "headphone-output";
+      preset-name = "empty";
+    };
+
+  xdg.configFile."easyeffects/autoload/output/bluez_output.98_8E_79_00_89_6C.1:headphone-hf-output.json".text =
+    builtins.toJSON {
+      device = "bluez_output.98_8E_79_00_89_6C.1";
+      device-description = "Qudelix-5K";
+      device-profile = "headphone-hf-output";
+      preset-name = "empty";
+    };
+
+  xdg.configFile."easyeffects/autoload/output/alsa_output.usb-QTIL_Qudelix-5K_USB_DAC_MIC_44.1KHz_ABCDEF0123456789-00.analog-stereo:analog-output.json".text =
+    builtins.toJSON {
+      device =
+        "alsa_output.usb-QTIL_Qudelix-5K_USB_DAC_MIC_44.1KHz_ABCDEF0123456789-00.analog-stereo";
+      device-description = "Qudelix-5K USB DAC/MIC 44.1KHz Analog Stereo";
+      device-profile = "analog-output";
       preset-name = "empty";
     };
 }
