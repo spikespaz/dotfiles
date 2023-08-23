28a0947983d1b34d6aaa95d0520dbc89f8d1cc2b
Jacob Birkett <spikespaz@outlook.com>
2023-08-23 01:23:22 -0700

    me: progs: zsh: set mainProgram for nvimpager'

--- users/jacob/programs/zsh.nix
+++ users/jacob/programs/zsh.nix
@@ -1,15 +1,18 @@
 args@{ self, config, lib, pkgs, ... }:
 let
   # FIXME git_colors based on git config
-  nvimpager' = let super = pkgs.nvimpager;
+  nvimpager' = let
+    super = pkgs.nvimpager;
+    mainProgram = super.meta.mainProgram or super.pname;
   in pkgs.symlinkJoin {
     name = super.name;
     paths = [ super ];
     nativeBuildInputs = super.nativeBuildInputs or [ ] ++ [ pkgs.makeWrapper ];
     postBuild = super.postInstall or "" + ''
-      wrapProgram $out/bin/${super.meta.mainProgram or super.pname} \
+      wrapProgram $out/bin/${mainProgram} \
         --add-flags '-- -c "lua nvimpager.maps = false; nvimpager.git_colors = true"'
     '';
+    meta = super.meta // { inherit mainProgram; };
   };
 in {
   imports = [ self.homeManagerModules.zsh ];
