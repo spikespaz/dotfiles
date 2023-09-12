ce73b958bc3cef7b09ca1d1a4e223444b259af3e
Jacob Birkett <spikespaz@outlook.com>
2023-09-11 18:12:46 -0700

    me: progs: armcord: create gruvbox-darker theme

--- users/jacob/programs/discord/armcord.nix
+++ users/jacob/programs/discord/armcord.nix
@@ -4,9 +4,21 @@ let
   configDir = "ArmCord";
 
   # <https://github.com/mwittrien/BetterDiscordAddons/blob/master/Themes/DiscordRecolor/DiscordRecolor.theme.css>
-  # defaultTheme = colorSchemes.default // themeSettings.default;
+  # defaultTheme = colorSchemes.default // {
+  #   default = {
+  #     font = [
+  #       "gg sans"
+  #       "Noto Sans"
+  #       "Helvetica Neue"
+  #       "Helvetica"
+  #       "Arial"
+  #       "sans-serif"
+  #     ];
+  #     settingsicons = true;
+  #   };
+  # };
 
-  theme = colorSchemes.default // {
+  theme = colorSchemes.gruvbox-darker // {
     font = [ "system-ui" ];
     settingsicons = false;
   };
@@ -35,19 +47,31 @@ let
       backgroundtertiary = [ 10 10 10 ];
       backgroundfloating = [ 0 0 0 ];
     };
-  };
+    gruvbox-darker = let
+      gb = builtins.mapAttrs (_: { r, g, b }: [ r g b ])
+        lib.birdos.colors.palettes.gruvbox.dark;
+    in {
+      accentcolor = gb.hl_orange;
+      accentcolor2 = gb.fg_purple;
+      linkcolor = gb.fg_blue;
+      mentioncolor = gb.fg_aqua;
+      successcolor = gb.hl_green;
+      warningcolor = gb.hl_yellow;
+      dangercolor = gb.hl_red;
 
-  themeSettings = {
-    default = {
-      font = [
-        "gg sans"
-        "Noto Sans"
-        "Helvetica Neue"
-        "Helvetica"
-        "Arial"
-        "sans-serif"
-      ];
-      settingsicons = true;
+      textbrightest = gb.fg0;
+      textbrighter = gb.fg0;
+      textbright = gb.fg1;
+      textdark = gb.bg4;
+      textdarker = gb.bg3;
+      textdarkest = gb.bg2;
+
+      backgroundaccent = gb.bg0_soft;
+      backgroundprimary = [ 20 20 20 ];
+      backgroundsecondary = [ 16 16 16 ];
+      backgroundsecondaryalt = [ 12 12 12 ];
+      backgroundtertiary = [ 10 10 10 ];
+      backgroundfloating = [ 0 0 0 ];
     };
   };
 
