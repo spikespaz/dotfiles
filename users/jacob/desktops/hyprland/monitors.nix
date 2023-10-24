49c299b387101f3a7f26dd13cf3d822e1efdbaa0
Jacob Birkett <spikespaz@outlook.com>
2023-10-23 23:06:18 -0700

    me: hl: monitors: remove fallback config

--- users/jacob/desktops/hyprland/monitors.nix
+++ users/jacob/desktops/hyprland/monitors.nix
@@ -2,12 +2,10 @@
 let
   monitors = {
     internal = "eDP-1";
-    hotplug = "HDMI-A-1";
     portable = "DP-2";
     dock = "DP-1";
+    hotplug = "HDMI-A-1";
   };
-
-  # hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
 in {
   wayland.windowManager.hyprland = with monitors; {
     # <https://wiki.hyprland.org/Configuring/Monitors/>
@@ -20,6 +18,19 @@ in {
       "${hotplug}, preferred, 0x1080, 1"
     ];
 
+    # Unfortunately, fallbacks do not work.
+    #
+    # I had expected to be able to do something like
+    # `"20".monitor = [ hotplug portable dock ];` to have the workspace
+    # assigned to a monitor depending on what is connected.
+    #
+    # The way that it is right now, when monitors are missing I believe
+    # the workspace positions are indeterminate, which is unfortunate.
+    #
+    # Eventually, I will solve this problem by completing
+    # <https://github.com/spikespaz/hyprland-workspace-profiles>.
+    #
+    # Such a feature has been explicitly declined for inclusion in Hyprland.
     workspaceRules = with monitors; {
       "1".monitor = internal;
       "3".monitor = internal;
@@ -27,31 +38,23 @@ in {
       "7".monitor = internal;
       "9".monitor = internal;
 
-      "2".monitor = [ portable dock ];
-      "4".monitor = [ portable dock ];
-      "6".monitor = [ portable dock ];
-      "8".monitor = [ portable dock ];
-      "10".monitor = [ portable dock ];
+      "2".monitor = dock;
+      "4".monitor = dock;
+      "6".monitor = dock;
+      "8".monitor = dock;
+      "10".monitor = dock;
 
-      "11".monitor = [ hotplug internal ];
-      "13".monitor = [ hotplug internal ];
-      "15".monitor = [ hotplug internal ];
-      "17".monitor = [ hotplug internal ];
-      "19".monitor = [ hotplug internal ];
+      "11".monitor = hotplug;
+      "13".monitor = hotplug;
+      "15".monitor = hotplug;
+      "17".monitor = hotplug;
+      "19".monitor = hotplug;
 
-      "12".monitor = [ hotplug portable dock ];
-      "14".monitor = [ hotplug portable dock ];
-      "16".monitor = [ hotplug portable dock ];
-      "18".monitor = [ hotplug portable dock ];
-      "20".monitor = [ hotplug portable dock ];
+      "12".monitor = portable;
+      "14".monitor = portable;
+      "16".monitor = portable;
+      "18".monitor = portable;
+      "20".monitor = portable;
     };
-
-    # eventListener.handler.monitorAdd = ''
-    #   ${hyprctl} dispatch moveworkspacetomonitor 2 $HL_MONITOR_NAME
-    #   ${hyprctl} dispatch moveworkspacetomonitor 4 $HL_MONITOR_NAME
-    #   ${hyprctl} dispatch moveworkspacetomonitor 6 $HL_MONITOR_NAME
-    #   ${hyprctl} dispatch moveworkspacetomonitor 8 $HL_MONITOR_NAME
-    #   ${hyprctl} dispatch moveworkspacetomonitor 10 $HL_MONITOR_NAME
-    # '';
   };
 }
