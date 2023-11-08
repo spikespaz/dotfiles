24c570769b195dcf603f1993d4d61ef369ba0bb2
Jacob Birkett <jacob@birkett.dev>
2023-11-08 11:44:15 -0700

    rebase

--- hm-modules/keepassxc.nix
+++ hm-modules/keepassxc.nix
@@ -1,8 +1,14 @@
+# TODO maybe?
+# <https://gist.github.com/dAnjou/b99f55de34b90246f381e71e3c8f9262>
+# <https://github.com/keepassxreboot/keepassxc/issues/613>
 { config, lib, pkgs, ... }:
 let
   inherit (lib) types;
+
   service = config.services.keepassxc;
   program = config.programs.keepassxc;
+
+  iniFormat = pkgs.formats.ini { };
 in {
   options = {
     services.keepassxc = {
@@ -25,6 +31,14 @@ in {
       '');
 
       package = lib.mkPackageOption pkgs "keepassxc" { };
+
+      settings = lib.mkOption {
+        type = iniFormat.type;
+        default = { };
+        description = lib.mdDoc ''
+          Settings to write in INI format to {file}`~/.config/keepassxc/keepassxc.ini`.
+        '';
+      };
     };
   };
 
@@ -32,6 +46,11 @@ in {
     (lib.mkIf program.enable { home.packages = [ program.package ]; })
 
     { services.keepassxc.package = lib.mkDefault program.package; }
+
+    (lib.mkIf (program.enable && program.settings != { }) {
+      xdg.configFile."keepassxc/keepassxc.ini".source =
+        iniFormat.generate "keepassxc.ini" program.settings;
+    })
 
     (lib.mkIf service.enable {
       systemd.user.services.keepassxc = {
