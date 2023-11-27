9ccd1b25e4759937f572dab544fcdfec128c6dcf
Jacob Birkett <jacob@birkett.dev>
2023-11-27 00:49:45 -0700

    me: vscode: settings: fix for schema change

--- users/jacob/programs/vscode/settings.nix
+++ users/jacob/programs/vscode/settings.nix
@@ -150,7 +150,7 @@
     "terminal.integrated.gpuAcceleration" = "on";
 
     # hide the action bar, I know the keybinds
-    "workbench.activityBar.visible" = false;
+    "workbench.activityBar.location" = "hidden";
     # put the sidebar on the right so that text doesn't jump
     "workbench.sideBar.location" = "right";
     # no delay when automatically hiding the sidebar or panels
