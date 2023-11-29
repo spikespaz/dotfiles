e4315ad46f37a7718057348aac94ebfba0c9f85a
Jacob Birkett <jacob@birkett.dev>
2023-11-29 07:04:43 -0700

    me@odsy: hl: monitors: relative monitor positions

--- users/jacob@odyssey/hyprland/monitors.nix
+++ users/jacob@odyssey/hyprland/monitors.nix
@@ -1,27 +1,84 @@
-# FIXME Only `mkForce` for now because the `users/jacob` dir needs to all be `mkDefault`.
+# The monitor and workspace positions are entirely dependent upon the order of
+# which I plug in the displays. The portable monitor has to be plugged in
+# with Thunderbolt before connecting the laptop to the dock.
+# If the portable monitor isn't connected to the dock before the laptop,
+# it will switch DP-5 and DP-6.
+#
+# TODO: Look into at a feature addition for Hyprland where
+# monitor descriptions can be matched.
+# Also could be fixed in HLWSP.
+#
 { lib, pkgs, config, ... }:
 let
-  monitors = {
-    internal = "eDP-1";
-    portable = "DP-5";
-    dock = "DP-6";
-    hotplug = "HDMI-A-1";
-  };
   hyprctl =
     lib.getExe' config.wayland.windowManager.hyprland.finalPackage "hyprctl";
+
+  origin = {
+    x = 0;
+    y = 0;
+  };
+  inherit (config.wayland.windowManager.hyprland.monitors)
+    portable internal desk-dock hotplug;
 in {
   home.packages = [ pkgs.wdisplays ];
 
-  wayland.windowManager.hyprland = with monitors; {
-    # <https://wiki.hyprland.org/Configuring/Monitors/>
-    config.monitor = [
-      ", preferred, auto, 1"
+  wayland.windowManager.hyprland = {
+    monitors = {
+      # The laptop is on an incline, positioned directly beneath the
+      # monitor affixed to my desk.
+      # The internal Samsung display (for ThinkPad P14s) is 3.8k 16:10 OLED.
+      # scaled to a virtual size of 1920x1080.
+      internal = {
+        name = "eDP-1";
+        resolution.x = 2880;
+        resolution.y = 1800;
+        scale = 1.5;
+        # There is a portable monitor sometimes plugged in to my lower-left.
+        position.x = portable.position.x + builtins.floor portable.size.x;
+        position.y = desk-dock.position.y + builtins.floor desk-dock.size.y;
+        refreshRate = 90;
+        bitdepth = 10;
+      };
 
-      "${internal}, preferred, 1920x1080, 1.5"
-      "${portable}, preferred, 0x1080, 1"
-      "${dock}, preferred, 1920x0, 1"
-      "${hotplug}, preferred, 0x1080, 1"
-    ];
+      # I have a 2k 16:9 monitor on a mount at head level, directly forward.
+      desk-dock = {
+        name = "DP-5";
+        resolution.x = 2560;
+        resolution.y = 1440;
+        # Align it directly above the internal monitor,
+        # overhanging the portable monitor underneath to the left.
+        position.x = builtins.floor
+          ( # Middle X coordinate of the internal monitor,
+            (internal.position.x + internal.size.x / 2)
+            # subtracted by half the scaled width of this monitor.
+            - desk-dock.size.x / 2);
+        position.y = origin.y;
+        refreshRate = 144;
+        bitdepth = 10;
+      };
+
+      # The portable monitor is on my lower-left,
+      # just under the bottom corner of the desk monitor
+      # and adjacent left of the internal one.
+      portable = {
+        name = "DP-6";
+        resolution.x = 1920;
+        resolution.y = 1080;
+        position.x = origin.x;
+        position.y = desk-dock.position.y + builtins.floor desk-dock.size.y;
+        bitdepth = 10;
+      };
+
+      # A random HDMI monitor gets assigned to the right of the internal one.
+      hotplug = {
+        name = "HDMI-A-1";
+        position.x = builtins.floor (internal.position.x + internal.size.x);
+        position.y = internal.position.y;
+      };
+    };
+
+    # This is just a default.
+    config.monitor = [ ", preferred, auto, 1" ];
 
     # Unfortunately, fallbacks do not work.
     #
@@ -36,30 +93,31 @@ in {
     # <https://github.com/spikespaz/hyprland-workspace-profiles>.
     #
     # Such a feature has been explicitly declined for inclusion in Hyprland.
-    workspaceRules = with monitors; {
-      "1".monitor = internal;
-      "3".monitor = internal;
-      "5".monitor = internal;
-      "7".monitor = internal;
-      "9".monitor = internal;
+    workspaceRules = {
+      "1".monitor = internal.name;
+      "3".monitor = internal.name;
+      "5".monitor = internal.name;
+      "7".monitor = internal.name;
+      "9".monitor = internal.name;
 
-      "2".monitor = dock;
-      "4".monitor = dock;
-      "6".monitor = dock;
-      "8".monitor = dock;
-      "10".monitor = dock;
+      "2".monitor = desk-dock.name;
+      "4".monitor = desk-dock.name;
+      "6".monitor = desk-dock.name;
+      "8".monitor = desk-dock.name;
+      "10".monitor = desk-dock.name;
 
-      "11".monitor = hotplug;
-      "13".monitor = hotplug;
-      "15".monitor = hotplug;
-      "17".monitor = hotplug;
-      "19".monitor = hotplug;
+      "11".monitor = portable.name;
+      "13".monitor = portable.name;
+      "15".monitor = portable.name;
+      "17".monitor = portable.name;
+      "19".monitor = portable.name;
 
-      "12".monitor = portable;
-      "14".monitor = portable;
-      "16".monitor = portable;
-      "18".monitor = portable;
-      "20".monitor = portable;
+      "12".monitor = hotplug.name;
+      "14".monitor = hotplug.name;
+      "16".monitor = hotplug.name;
+      "18".monitor = hotplug.name;
+      "20".monitor = hotplug.name;
+
     };
 
     # This is here to fix a Hyprland bug that seems to persist (repeat regression).
