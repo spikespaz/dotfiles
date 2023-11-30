6dd4560e9c2b94b0da42eb3104f32adf7e201799
Jacob Birkett <jacob@birkett.dev>
2023-11-30 06:10:59 -0700

    me@odsy: hyprland: set workspace swipe force speed

--- users/jacob@odyssey/hyprland/config.nix
+++ users/jacob@odyssey/hyprland/config.nix
@@ -1,5 +1,7 @@
-{ ... }: {
+{ lib, ... }: {
   wayland.windowManager.hyprland.config = {
     xwayland.force_zero_scaling = true;
+
+    gestures.workspace_swipe = { min_speed_to_force = lib.mkForce 10; };
   };
 }
--- users/jacob@odyssey/hyprland/default.nix
+++ users/jacob@odyssey/hyprland/default.nix
@@ -1,1 +1,1 @@
-{ pkgs, ... }: { imports = [ ./config.nix ./monitors.nix ]; }
+{ imports = [ ./config.nix ./monitors.nix ]; }
