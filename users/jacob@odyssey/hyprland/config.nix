cd8dc9e129983aa66e6c94b1836955b41e81ffed
Jacob Birkett <jacob@birkett.dev>
2023-12-01 07:34:29 -0700

    me@odsy: hl: config: update touchpad settings

--- users/jacob@odyssey/hyprland/config.nix
+++ users/jacob@odyssey/hyprland/config.nix
@@ -1,6 +1,14 @@
 { lib, ... }: {
   wayland.windowManager.hyprland.config = {
     xwayland.force_zero_scaling = true;
+    # xwayland.use_nearest_neighbor = true;
+
+    input.touchpad = {
+      scroll_factor = 0.75;
+      tap_to_click = false;
+      tap_and_drag = true;
+      clickfinger_behavior = true;
+    };
 
     gestures.workspace_swipe = { min_speed_to_force = lib.mkForce 10; };
   };
