89ad2c9eb13ba3284ee8a79be7779f44d48765b3
Jacob Birkett <jacob@birkett.dev>
2023-12-01 07:06:58 -0700

    secrets: re-key with root@odyssey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,17 @@
 let
-  intrepid = {
-    root =
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtXAJRcYBrVT8qAoc+bq6ZoPr3ehypwx0BohKv8HWMF root@intrepid";
-    jacob =
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHK69yW51FZuJB9MuLY/qzcVp/yXK/7DaFliqjYN/Ad7 jacob@intrepid";
-  };
+  jacob =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHK69yW51FZuJB9MuLY/qzcVp/yXK/7DaFliqjYN/Ad7 jacob";
 
-  root = [ intrepid.root ];
-  users = [ intrepid.jacob ];
+  intrepid =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtXAJRcYBrVT8qAoc+bq6ZoPr3ehypwx0BohKv8HWMF root@intrepid";
+
+  odyssey =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKReH8gif/uzkgG8CaUu6GoVaft9gNhSLLzF36cBT6NX root@odyssey";
+
+  hosts = [ intrepid odyssey ];
+  users = [ jacob ];
 in {
-  "root.pia-user-pass.age".publicKeys = root;
-  "root.nix-access-tokens-github.age".publicKeys = root;
+  "root.pia-user-pass.age".publicKeys = hosts;
+  "root.nix-access-tokens-github.age".publicKeys = hosts;
   "jacob.spotifyd.age".publicKeys = users;
 }
