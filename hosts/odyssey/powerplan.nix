5e2ae5fc3b56eb94f3e8d349fb20914839b205b5
Jacob Birkett <jacob@birkett.dev>
2023-12-06 05:48:28 -0700

    hosts: odsy: pplan: configure amd_pstate_epp powersave balance_performance
    Ref: <https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html>
    
    gov = powersave
    epp = balance_performance

--- hosts/odyssey/powerplan.nix
+++ hosts/odyssey/powerplan.nix
@@ -2,13 +2,51 @@
 # - <https://www.kernel.org/doc/Documentation/power/states.txt>
 # - `man sleep.conf.d`
 # - `man logind.conf`
-{ self, ... }:
+{ self, config, ... }:
 let
   # idleAfter = 5 * 60; # seconds
   hibernateDelay = 1.5 * 60 * 60; # seconds
   # MHz = x: x * 1000;
 in {
   # imports = [ self.nixosModules.amdctl ];
+
+  environment.systemPackages = [
+    # *cpupower* could be in `packages.nix`, but it's a common diagnostic
+    # tool that should probably come along with the config that follows.
+    # <https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html#cpupower-tool-support-for-amd-pstate>
+    config.boot.kernelPackages.cpupower
+  ];
+
+  boot.kernelParams = [
+    # Required reading:
+    # <https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html>
+    # <https://www.kernel.org/doc/html/latest/admin-guide/pm/amd-pstate.html#amd-pstate-driver-operation-modes>
+    #
+    # Note that the EPP driver succeeds TLP's scheduler switching
+    # and makes it obsolete. Disable that function of TLP.
+    "amd_cpufreq=active"
+  ];
+
+  # This CPU (R7 8740U) has these energy performance preferences:
+  # default, performance, balance_performance, balance_power, power.
+  #
+  # Based on Phoronix' results:
+  # <https://www.phoronix.com/review/amd-pstate-epp-ryzen-mobile>
+  #
+  # I have decided on scheduler `powersave` and EPP `balance_performance`.
+  # I will also consider `powersave` and ` balance_power` once we know more.
+  # <https://www.phoronix.com/review/amd-pstate-epp-ryzen-mobile>
+  #
+  # I assume `balance_performance` means to choose to balance
+  # performance over power efficiency,
+  # and `balance_power` chooses efficiency over performance.
+  #
+  # FIXME
+  # Because I am unsure, I will leave that for foture investigation.
+  services.udev.extraRules = ''
+    KERNEL=="cpu[0-15]", SUBSYSTEM=="cpu", ATTR{cpufreq/scaling_governor}="powersave"
+    KERNEL=="cpu[0-15]", SUBSYSTEM=="cpu", ATTR{cpufreq/energy_performance_preference}="balance_performance"
+  '';
 
   # services.tlp.enable = false;
   # services.auto-cpufreq.enable = true;
