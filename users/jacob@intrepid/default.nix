7cd4c29d8c643a1d66a2fe294138bd4e27cf5586
Jacob Birkett <jacob@birkett.dev>
2023-12-09 02:32:52 -0700

    me: disable Home Manager news

--- users/jacob@intrepid/default.nix
+++ users/jacob@intrepid/default.nix
@@ -1,15 +1,24 @@
 args@{ self, tree, lib, inputs }:
-lib.bird.mkHome args rec {
+let
+  # It doesn't even work out of the box with flakes...
+  # <https://github.com/nix-community/home-manager/issues/2033#issuecomment-1801557851>
+  #
+  # Include this in the `modules` passed to
+  # `inputs.home-manager.lib.homeManagerConfiguration`.
+  disableHomeManagerNews = {
+    # disabledModules = [ "misc/news.nix" ];
+    config = {
+      news.display = "silent";
+      news.json = lib.mkForce { };
+      news.entries = lib.mkForce [ ];
+    };
+  };
+in lib.bird.mkHome args rec {
   hostPlatform.system = "x86_64-linux";
   nixpkgs = inputs.nixpkgs-unstable;
   nixpkgsArgs.config.allowUnfree = true;
   modules = with tree.users.jacob; [
-    # It doesn't even work out of the box with flakes...
-    # https://github.com/nix-community/home-manager/issues/2033#issuecomment-1801557851
-    {
-      # news.display = "silent";
-      disabledModules = [ "misc/news.nix" ];
-    }
+    disableHomeManagerNews
     profile
     wayland.default
     wayland.suite # not included in default
