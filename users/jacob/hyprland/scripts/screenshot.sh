78c768e7fd36ef00dff9c1a1ce8061b545da6b3f
Jacob Birkett <jacob@birkett.dev>
2024-02-29 15:03:49 -0700

    me: hl: scripts: screenshot: save as temp file

--- users/jacob/hyprland/scripts/screenshot.sh
+++ users/jacob/hyprland/scripts/screenshot.sh
@@ -4,22 +4,36 @@
 
 set -eu -o pipefail
 
-declare -A bounds
+declare -A window
 while IFS='=' read -r key value; do
-  bounds["$key"]="$value"
+  window["$key"]="$value"
 done < <(
 	hyprctl -j activewindow \
-	| jq -r '{x: .at[0], y: .at[1], w: .size[0], h: .size[1]} | to_entries | .[] | "\(.key)=\(.value)"'
+	| jq -r '{x: .at[0], y: .at[1], w: .size[0], h: .size[1], title: .initialTitle} | to_entries | .[] | "\(.key)=\(.value)"'
 )
 
-echo "x: ${bounds["x"]}"
-echo "y: ${bounds["y"]}"
-echo "w: ${bounds["w"]}"
-echo "h: ${bounds["h"]}"
+echo "title: ${window['title']}"
+echo "x: ${window['x']}"
+echo "y: ${window['y']}"
+echo "w: ${window['w']}"
+echo "h: ${window['h']}"
+
+time_date="$(date '+%Y-%m-%d_%H:%M:%S')"
+
+title="${window['title']}"
+title="${title//\'/}"
+title="${title//[[:space:]]/_}"
+title="${title//[^a-zA-Z0-9]/-}"
+
+image_name="${title}_$time_date.png"
+image_path="/tmp/$image_name"
+
+echo "image path: $image_path"
 
 grim \
 	-c \
 	-t png \
-	-g "${bounds["x"]},${bounds["y"]} ${bounds["w"]}x${bounds["h"]}" \
-	- | wl-copy -t image/png
+	-g "${window["x"]},${window["y"]} ${window["w"]}x${window["h"]}" \
+	"$image_path"
 
+wl-copy -t image/png < "$image_path"
