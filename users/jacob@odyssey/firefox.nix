eaa365ce4437fe462c881657c5dc91575cc86dc4
Jacob Birkett <jacob@birkett.dev>
2024-02-29 14:46:36 -0700

    me: firefox: wavefox: add version hash for 1.6.123

--- users/jacob@odyssey/firefox.nix
+++ users/jacob@odyssey/firefox.nix
@@ -6,6 +6,7 @@ let
     hashes = {
       "1.6.121" = "sha256-YV6d/yYC42EmN8fVMvC95GSNqUWrCuS5tdHpv+1+C1U=";
       "1.6.122" = "sha256-29LleIJ+c9HYyxlE59pP09OMzPKcx2JDeidZcBOs6+0=";
+      "1.6.123" = "sha256-uVGNJKtT8MHo5a+GTW6DfpuRiCukC4e4UdnKmWIk3Zw=";
     };
 
     firefoxVer = config.programs.firefox.package.version;
