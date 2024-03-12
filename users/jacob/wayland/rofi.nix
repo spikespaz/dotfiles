28922291037ebf5f048d9d8bf5bc3505594d3925
Jacob Birkett <jacob@birkett.dev>
2024-03-12 16:45:44 -0700

    me: wl: rofi: replace theme with lib.colors use

--- users/jacob/wayland/rofi.nix
+++ users/jacob/wayland/rofi.nix
@@ -25,44 +25,6 @@
       })
     ];
     theme = let
-      gruvbox = {
-        normal = {
-          orange = "#d65d0e";
-          red = "#cc241d";
-          green = "#98971a";
-          yellow = "#d79921";
-          blue = "#458588";
-          purple = "#b16286";
-          aqua = "#689d6a";
-          gray = "#a89984";
-        };
-        bright = {
-          red = "#fb4934";
-          green = "#b8bb26";
-          yellow = "#fabd2f";
-          blue = "#83a598";
-          purple = "#d3869b";
-          aqua = "#8ec07c";
-          orange = "#fe8019";
-          gray = "#928374";
-        };
-        bg = gruvbox.bg0;
-        bg0 = "#282828";
-        bg0_h = "#1d2021";
-        bg0_s = "#32302f";
-        bg1 = "#3c3836";
-        bg2 = "#504945";
-        bg3 = "#665c54";
-        bg4 = "#7c6f64";
-        fg = gruvbox.fg1;
-        fg0 = "#fbf1c7";
-        fg1 = "#ebdbb2";
-        fg2 = "#d5c4a1";
-        fg3 = "#bdae93";
-        fg4 = "#a89984";
-        inherit (gruvbox.bright) orange red green yellow blue purple aqua gray;
-      };
-
       # Use `mkLiteral` for string-like values that should show without
       # quotes, e.g.:
       # {
@@ -71,18 +33,12 @@
       # };
       inherit (config.lib.formats.rasi) mkLiteral;
 
-      bgOpacity = "e5"; # 90%
-
-      bg0 = mkLiteral (gruvbox.bg0_h + bgOpacity);
-      bg1 = mkLiteral (gruvbox.bg0_s + bgOpacity);
-      # selected gets no opacity
-      bg2 = mkLiteral gruvbox.normal.orange;
-
-      fg0 = mkLiteral gruvbox.fg1;
-      fg1 = mkLiteral gruvbox.fg2;
-      fg2 = mkLiteral gruvbox.fg3;
+      inherit (lib.birdos.colors) hexRGBA';
+      mkColorLiteral = rgb: a: mkLiteral (hexRGBA' rgb a);
+      gb = (lib.birdos.colors.formats.custom mkColorLiteral).gruvbox.dark;
 
       font = "Ubuntu Regular";
+      accent = gb.hl_orange;
     in {
       #*****************************************************************************
       # MACOS SPOTLIGHT LIKE DARK THEME FOR ROFI
@@ -94,7 +50,7 @@
         font = "${font} 12";
 
         background-color = mkLiteral "transparent";
-        text-color = fg0;
+        text-color = gb.fg0 1.0;
 
         margin = mkLiteral "0";
         padding = mkLiteral "0";
@@ -102,7 +58,7 @@
       };
 
       window = {
-        background-color = bg0;
+        background-color = gb.bg0_hard 0.9;
 
         location = mkLiteral "center";
         width = mkLiteral "640";
@@ -131,13 +87,13 @@
         font = mkLiteral "inherit";
 
         placeholder = "Search";
-        placeholder-color = fg2;
+        placeholder-color = gb.fg3 1.0;
       };
 
       message = {
         border = mkLiteral "2px 0 0";
-        border-color = bg1;
-        background-color = bg1;
+        border-color = gb.bg0_soft 0.9;
+        background-color = gb.bg0_soft 0.9;
       };
 
       textbox = { padding = mkLiteral "8px 24px"; };
@@ -148,7 +104,7 @@
 
         fixed-height = mkLiteral "false";
         border = mkLiteral "1px 0 0";
-        border-color = bg1;
+        border-color = gb.bg0_soft 0.9;
       };
 
       element = {
@@ -157,11 +113,11 @@
         background-color = mkLiteral "transparent";
       };
 
-      "element normal active" = { text-color = bg2; };
+      "element normal active" = { text-color = accent 1.0; };
 
       "element selected normal, element selected active" = {
-        background-color = bg2;
-        text-color = bg0;
+        background-color = accent 1.0;
+        text-color = gb.bg0_hard 0.9;
       };
 
       element-icon = { size = mkLiteral "1em"; };
