e5feff720782c487e928f35c0162349a73231dc6
Jacob Birkett <jacob@birkett.dev>
2024-03-30 10:05:29 -0700

    odyssey: hardware: explicitly set RADV and radeonsi

--- hosts/odyssey/misc.nix
+++ hosts/odyssey/misc.nix
@@ -155,6 +155,22 @@
       extraPackages = [ pkgs.amdvlk ];
       extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
     };
+
+    environment.variables = {
+      # Not sure if this should be necessary, but enabling RADV did not work.
+      VK_ICD_FILENAMES = lib.concatStringsSep ":" [
+        "/run/opengl-driver/share/vulkan/icd.d/amd_icd64.json"
+        "/run/opengl-driver-32/share/vulkan/icd.d/amd_icd32.json"
+        "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json"
+        "/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json"
+      ];
+      # From Mesa, for Vulkan, alongside `radeonsi` for OpenGL.
+      AMD_VULKAN_ICD = "RADV";
+      # Mesa Gallium driver, OpenGL to Vulkan. Third-layer abstraction.
+      # MESA_LOADER_DRIVER_OVERRIDE = "zink";
+      # Official AMD, directly provides OpenGL. Use RADV for Vulkan.
+      MESA_LOADER_DRIVER_OVERRIDE = "radeonsi";
+    };
   }
 
   ################
