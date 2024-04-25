2f0f3fb7ea11e3ed7fc447070f52e703590fcde4
Jacob Birkett <jacob@birkett.dev>
2024-04-24 19:44:05 -0700

    packages: java: rework graalvm source defs

--- packages/java/default.nix
+++ packages/java/default.nix
@@ -1,37 +1,52 @@
-{ lib, pkgs, fetchurl, callPackage, graalvmCEPackages }:
+{ lib, pkgs, fetchurl, graalvmCEPackages
+, systems ? [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ]
+}:
 let
+  temurinSources = lib.importJSON ./temurin-sources.json;
+  graalvmCeSources = lib.importJSON ./graalvm-ce-sources.json;
+
   mkJava = opts:
     pkgs.callPackage (import
       "${pkgs.path}/pkgs/development/compilers/temurin-bin/jdk-linux-base.nix"
       opts) { };
 
-  temurinSources = lib.importJSON ./temurin-sources.json;
+  mkBorkedGraalVmCe = { url, sha256, version, javaVersion }:
+    (graalvmCEPackages.buildGraalvm {
+      inherit version;
+      javaVersion = toString javaVersion;
+      src = fetchurl { inherit url sha256; };
+      meta.platforms = systems;
+    }).overrideAttrs (self: super: {
+      doInstallCheck = false;
+      # Make sure that `native-image` exists so that `wrapProgram`
+      # has something to do.
+      preInstall = ''
+        touch $out/bin/native-image
+        chmod +x $out/bin/native-image
+      '' + (super.preInstall or "");
+      # Remove the `native-image` wrapper and the original empty file.
+      postFixup = (super.postFixup or "") + ''
+        rm $out/bin/native-image*
+      '';
+    });
 
+  graalvmPlatform = let
+    os = if pkgs.hostPlatform.isDarwin then
+      "darwin"
+    else if pkgs.hostPlatform.isLinux then
+      "linux"
+    else
+      abort "GraalVM is not available for your OS";
+    arch = if pkgs.hostPlatform.isAarch64 then
+      "aarch64"
+    else if pkgs.hostPlatform.is64bit then
+      "amd64"
+    else
+      abort "GraalVM does not support your CPU architecture";
+  in "${os}-${arch}";
+
+in lib.makeExtensible (self: {
   temurin20-jre-bin = mkJava { sourcePerArch = temurinSources.openjdk20; };
-
-  graalvm8-ce = (graalvmCEPackages.buildGraalvm {
-    version = "21.3.1";
-    javaVersion = "8";
-    src = fetchurl {
-      url =
-        "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-21.3.1/graalvm-ce-java8-linux-amd64-21.3.1.tar.gz";
-      sha256 = "sha256-uey9VC3h7Qo9pGpinyJmqIIDJpj1/LxU2JI3K5GJsO0=";
-    };
-    meta.platforms = [ "x86_64-linux" ];
-  }).overrideAttrs (self: super: {
-    doInstallCheck = false;
-    # Make sure that `native-image` exists so that `wrapProgram`
-    # has something to do.
-    preInstall = ''
-      touch $out/bin/native-image
-      chmod +x $out/bin/native-image
-    '' + (super.preInstall or "");
-    # Remove the `native-image` wrapper and the original empty file.
-    postFixup = (super.postFixup or "") + ''
-      rm $out/bin/native-image*
-    '';
-  });
-in {
-  inherit temurin20-jre-bin graalvm8-ce;
-  graalvm8-ce-jre = "${graalvm8-ce}/jre";
-}
+  graalvm8-ce = mkBorkedGraalVmCe graalvmCeSources.java8.${graalvmPlatform};
+  graalvm8-ce-jre = "${self.graalvm8-ce}/jre";
+})
