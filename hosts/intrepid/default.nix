4e37d6990fa5554fb9df1b6c0c309f7bf81eeae6
Jacob Birkett <jacob@birkett.dev>
2024-04-25 15:08:02 -0700

    hosts: intrepid: default: set nixpkgs host to znver2

--- hosts/intrepid/default.nix
+++ hosts/intrepid/default.nix
@@ -1,6 +1,8 @@
 args@{ self, tree, lib, inputs }:
 lib.bird.mkHost args {
-  hostPlatform.system = "x86_64-linux";
+  hostPlatform = {
+    system = "x86_64-linux";
+  } // lib.systems.architectures.featureSupport "znver2";
   nixpkgs = inputs.nixpkgs-unstable;
   nixpkgsArgs.config.allowUnfree = true;
   modules = with tree.hosts; [
