1a8ff9abb56c62c282d4e41715614d4fce5ed2f6
Jacob Birkett <jacob@birkett.dev>
2024-06-19 18:24:09 -0700

    me: ff: wavefox: update to 1.6.128

--- users/jacob/programs/firefox/wavefox.nix
+++ users/jacob/programs/firefox/wavefox.nix
@@ -1,12 +1,12 @@
 { lib, stdenv, fetchFromGitHub }:
 stdenv.mkDerivation (self: {
   pname = "wavefox-userchrome";
-  version = "1.6.123";
+  version = "1.6.128";
   src = fetchFromGitHub {
     owner = "QNetITQ";
     repo = "WaveFox";
     rev = "v${self.version}";
-    hash = "sha256-uVGNJKtT8MHo5a+GTW6DfpuRiCukC4e4UdnKmWIk3Zw=";
+    hash = "sha256-HwofuoPoPZ+uuD9PVeHvxngRcBEAsSDccz1jWZWHFvI=";
   };
   installPhase = ''
     mkdir $out
