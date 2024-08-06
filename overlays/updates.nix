44821ddfe8c0fa14dd8dd11629125da8a3f51b26
Jacob Birkett <jacob@birkett.dev>
2024-08-06 13:12:41 -0700

    overlays: updates: update gallery-dl

--- overlays/updates.nix
+++ overlays/updates.nix
@@ -113,4 +113,14 @@ in {
         hash = "sha256-WXjPdGwwdO13vsyVYTuoLgMedsADS8aOvZL+cwccBWQ=";
       };
     });
+
+  gallery-dl = pkgs0.gallery-dl.overrideAttrs (self: super: {
+    version = "1.27.2";
+    src = pkgs.fetchFromGitHub {
+      owner = "mikf";
+      repo = "gallery-dl";
+      rev = "v${self.version}";
+      hash = "sha256-gb2YeBIRCH/QfF7QhkvGuCWjquvU1WmsIwxWUYGiQXA=";
+    };
+  });
 }
