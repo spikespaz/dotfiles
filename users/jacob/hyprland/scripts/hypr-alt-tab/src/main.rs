3dccc744b203ef713f9c2a991bba1cbf1fc77947
Jacob Birkett <jacob@birkett.dev>
2024-11-01 23:07:54 -0700

    me: hl: hypr-alt-tab: switch windows only on current workspace

--- users/jacob/hyprland/scripts/hypr-alt-tab/src/main.rs
+++ users/jacob/hyprland/scripts/hypr-alt-tab/src/main.rs
@@ -2,15 +2,36 @@ use hyprland::data::{Client, Clients};
 use hyprland::dispatch::{Dispatch, DispatchType, WindowIdentifier, WindowSwitchDirection};
 use hyprland::shared::{HyprData, HyprDataActiveOptional};
 
+struct Settings {
+    /// If `true`, disables focusing/switching to windows on other workspaces.
+    current_workspace_only: bool,
+}
+
 fn main() -> anyhow::Result<()> {
+    let settings = Settings {
+        current_workspace_only: true,
+    };
+
     if let Some(active_client) = Client::get_active()? {
         if active_client.floating {
             // Move the current (topmost) floating window below another (if there is another).
-            Dispatch::call(DispatchType::Custom("alterzorder", "bottom, activewindow"))?;
+            Dispatch::call(DispatchType::Custom(
+                "alterzorder",
+                &format!("bottom, {}", active_client.address),
+            ))?;
 
-            // Find the new topmost floating window and focus it.
-            let floating_clients = Clients::get()?.into_iter().filter(|c| c.floating);
-            // https://github.com/hyprwm/Hyprland/issues/8283
+            // Find the new topmost floating window.
+            // Also, if `current_workspace_only`, filter out windows from other workspaces.
+            let floating_clients = Clients::get()?.into_iter().filter(|c| {
+                c.floating
+                    && (!settings.current_workspace_only
+                        || (c.workspace.id == active_client.workspace.id))
+            });
+
+            // According to Vaxry in the issue linked below,
+            // the topmost floating window is going to be the last in the JSON list.
+            // I say this is an implementation detail, but it's what we have to work with.
+            // <https://github.com/hyprwm/Hyprland/issues/8283>
             if let Some(topmost) = floating_clients.last() {
                 Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::Address(
                     topmost.address,
