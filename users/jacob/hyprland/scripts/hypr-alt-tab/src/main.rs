c424772e21ff7751402e28f222543e74201a0ea1
Jacob Birkett <jacob@birkett.dev>
2024-11-04 22:46:58 -0700

    me: hl: hypr-alt-tab: extract fn change_floating_active, less work

--- users/jacob/hyprland/scripts/hypr-alt-tab/src/main.rs
+++ users/jacob/hyprland/scripts/hypr-alt-tab/src/main.rs
@@ -12,43 +12,65 @@ fn main() -> anyhow::Result<()> {
         current_workspace_only: true,
     };
 
-    if let Some(active_client) = Client::get_active()? {
-        if active_client.floating {
-            // Move the current (topmost) floating window below another (if there is another).
-            Dispatch::call(DispatchType::Custom(
-                "alterzorder",
-                &format!("bottom, {}", active_client.address),
-            ))?;
-
-            // Find the new topmost floating window.
-            // Also, if `current_workspace_only`, filter out windows from other workspaces.
-            let floating_clients = Clients::get()?.into_iter().filter(|c| {
-                c.floating
-                    && (!settings.current_workspace_only
-                        || (c.workspace.id == active_client.workspace.id))
-            });
-
-            // According to Vaxry in the issue linked below,
-            // the topmost floating window is going to be the last in the JSON list.
-            // I say this is an implementation detail, but it's what we have to work with.
-            // <https://github.com/hyprwm/Hyprland/issues/8283>
-            if let Some(topmost) = floating_clients.last() {
-                Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::Address(
-                    topmost.address,
-                )))?;
-                Ok(())
-            } else {
-                unreachable!("if the active_client is floating, there is always at least one floating window")
-            }
-        } else if !active_client.grouped.is_empty() {
-            Dispatch::call(DispatchType::ChangeGroupActive(
-                WindowSwitchDirection::Forward,
-            ))?;
-            Ok(())
-        } else {
-            Ok(())
-        }
+    let active_client = if let Some(active_client) = Client::get_active()? {
+        active_client
     } else {
-        Ok(())
+        return Ok(());
+    };
+
+    if active_client.floating {
+        change_floating_active(Some(&active_client), settings.current_workspace_only)?;
+    } else if !active_client.grouped.is_empty() {
+        Dispatch::call(DispatchType::ChangeGroupActive(
+            WindowSwitchDirection::Forward,
+        ))?;
+    }
+
+    Ok(())
+}
+
+/// Like `changegroupactive` dispatcher but treats floating windows as "tabs" in a "group".
+///
+/// Returns the new active client (only if changed) upon success.
+fn change_floating_active(
+    active_client: Option<&Client>,
+    current_workspace_only: bool,
+) -> hyprland::Result<Option<Client>> {
+    let active_client_owned;
+
+    let active_client = if let Some(active_client) = active_client {
+        active_client
+    } else if let Some(active_client) = Client::get_active()? {
+        active_client_owned = active_client;
+        &active_client_owned
+    } else {
+        return Ok(None);
+    };
+
+    if !active_client.floating {
+        return Ok(None);
+    }
+
+    let mut floating_clients = Clients::get()?.into_iter().filter(|c| {
+        c.floating && (!current_workspace_only || (c.workspace.id == active_client.workspace.id))
+    });
+
+    // According to Vaxry in the issue linked below,
+    // the topmost floating window is going to be the last in the JSON list.
+    // I say this is an implementation detail, but it's what we have to work with.
+    // <https://github.com/hyprwm/Hyprland/issues/8283>
+    if let Some(focus_client) = floating_clients.nth_back(1) {
+        Dispatch::call(DispatchType::Custom(
+            "alterzorder",
+            &format!("bottom, {}", active_client.address),
+        ))?;
+
+        Dispatch::call(DispatchType::FocusWindow(WindowIdentifier::Address(
+            focus_client.address.clone(),
+        )))?;
+
+        Ok(Some(focus_client))
+    } else {
+        Ok(None)
     }
 }
