585bf699de010befdb866b0407cb09bfb0cb0244
Jacob Birkett <jacob@birkett.dev>
2025-04-05 12:12:22 -0700

    overlays: updates: rm old onlyoffice-bin_latest override

--- overlays/updates.nix
+++ overlays/updates.nix
@@ -1,16 +1,6 @@
 pkgs: pkgs0:
 let inherit (pkgs) lib;
 in {
-  onlyoffice-bin_latest = pkgs0.onlyoffice-bin_latest.overrideAttrs
-    (self: super: {
-      version = "8.0.1";
-      src = pkgs.fetchurl {
-        url =
-          "https://github.com/ONLYOFFICE/DesktopEditors/releases/download/v${self.version}/onlyoffice-desktopeditors_amd64.deb";
-        hash = "sha256-WXjPdGwwdO13vsyVYTuoLgMedsADS8aOvZL+cwccBWQ=";
-      };
-    });
-
   gallery-dl = pkgs0.gallery-dl.overrideAttrs (self: super: {
     version = "1.27.2";
     src = pkgs.fetchFromGitHub {
