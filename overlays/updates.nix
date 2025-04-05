ee1a8b898e309df70a0d009fea1a50ed444b1810
Jacob Birkett <jacob@birkett.dev>
2025-04-05 12:13:21 -0700

    overlays: updates: rm old gallery-dl override

--- overlays/updates.nix
+++ overlays/updates.nix
@@ -1,13 +1,1 @@
-pkgs: pkgs0:
-let inherit (pkgs) lib;
-in {
-  gallery-dl = pkgs0.gallery-dl.overrideAttrs (self: super: {
-    version = "1.27.2";
-    src = pkgs.fetchFromGitHub {
-      owner = "mikf";
-      repo = "gallery-dl";
-      rev = "v${self.version}";
-      hash = "sha256-gb2YeBIRCH/QfF7QhkvGuCWjquvU1WmsIwxWUYGiQXA=";
-    };
-  });
-}
+pkgs: pkgs0: let inherit (pkgs) lib; in { }
