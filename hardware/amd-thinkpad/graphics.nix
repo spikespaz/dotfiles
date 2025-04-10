1d3241c93b9ee4f20d621c954e109780ad431bf1
Jacob Birkett <jacob@birkett.dev>
2025-04-10 09:31:58 -0700

    graphics: ensure correct settings for switching RADV/AMDVLK

--- hardware/amd-thinkpad/graphics.nix
+++ hardware/amd-thinkpad/graphics.nix
@@ -4,6 +4,9 @@
     legacySupport.enable = true;
     # Ensure that AMDGPU is loaded over Radeon.
     initrd.enable = true;
+    # RADV is preferred for performance but `amdvlk` is fallback.
+    amdvlk.enable = true;
+    amdvlk.support32Bit.enable = true;
   };
 
   hardware.graphics = {
@@ -12,14 +15,16 @@
   };
 
   environment.variables = {
-    # Not sure if this should be necessary, but enabling RADV did not work.
-    VK_ICD_FILENAMES = lib.concatStringsSep ":" [
-      "/run/opengl-driver/share/vulkan/icd.d/amd_icd64.json"
-      "/run/opengl-driver-32/share/vulkan/icd.d/amd_icd32.json"
+    # Ordered.
+    # <https://gitlab.com/AndrewShark/amd-vulkan-prefixes/-/blob/f5fbf9a4625db2f53137c320ce82d8394c6b7ff8/amd_vulkan_prefixes.sh#L12>
+    VK_DRIVER_FILES = lib.concatStringsSep ":" [
       "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json"
       "/run/opengl-driver-32/share/vulkan/icd.d/radeon_icd.i686.json"
+      "/run/opengl-driver/share/vulkan/icd.d/amd_icd64.json"
+      "/run/opengl-driver-32/share/vulkan/icd.d/amd_icd32.json"
     ];
     # From Mesa, for Vulkan, alongside `radeonsi` for OpenGL.
+    # This environment variable can be changed to select `amdvlk`.
     AMD_VULKAN_ICD = "RADV";
     # Mesa Gallium driver, OpenGL to Vulkan. Third-layer abstraction.
     # MESA_LOADER_DRIVER_OVERRIDE = "zink";
