6c12e370692fb4065112f900e4068c4fe8f5747f
Jacob Birkett <jacob@birkett.dev>
2025-04-10 12:50:24 -0700

    me: wayland: kde-theme: finally fix materia-dark and kvantum

--- users/jacob/wayland/kde-theme.nix
+++ users/jacob/wayland/kde-theme.nix
@@ -1,19 +1,13 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, config, ... }:
+let inherit (pkgs) materia-kde-theme;
+in {
   qt.enable = true;
   qt.platformTheme.name = "qtct";
-  # qt.style.name = "kvantum";
-  home.packages = [ pkgs.libsForQt5.qt5ct ];
-
-  # The HM module seems to be broken when it sets this.
-  # Variables are `at5ct` for backwards-compatibility.
-  # home.sessionVariables.QT_STYLE_OVERRIDE = lib.mkForce "kvantum";
-  # systemd.user.sessionVariables.QT_STYLE_OVERRIDE = lib.mkForce "kvantum";
-  # home.sessionVariables.QT_QPA_PLATFORMTHEME = lib.mkForce "qt5ct";
-  # systemd.user.sessionVariables.QT_QPA_PLATFORMTHEME = lib.mkForce "qt5ct";
+  qt.style.name = "kvantum";
 
   programs.kvantum = {
     enable = true;
-    theme.package = pkgs.materia-kde-theme;
+    theme.package = materia-kde-theme;
     theme.name = "MateriaDark";
     theme.overrides = {
       General = {
@@ -33,4 +27,41 @@
       };
     };
   };
+
+  xdg.configFile."qt6ct/qt6ct.conf".text = lib.generators.toINI { } {
+    Appearance = {
+      color_scheme_path =
+        "${materia-kde-theme}/share/color-schemes/MateriaDark.colors";
+      custom_palette = true;
+      icon_theme = config.gtk.iconTheme.name;
+      standard_dialogs = "xdgdesktopportal";
+      style = "kvantum";
+    };
+
+    Fonts = {
+      fixed = "JetBrainsMono Nerd Font,12,-1,5,50,0,0,0,0,0,Regular";
+      general = "Ubuntu,12,-1,5,50,0,0,0,0,0,Regular";
+    };
+
+    Interface = let
+      KEYBOARD_SCHEME_KDE = 3;
+      FOLLOW_APPLICATION_STYLE = 4;
+    in {
+      activate_item_on_single_click = 1;
+      buttonbox_layout = 0;
+      cursor_flash_time = 1000;
+      dialog_buttons_have_icons = 1;
+      double_click_interval = 400;
+      gui_effects = "@Invalid()";
+      keyboard_scheme = KEYBOARD_SCHEME_KDE;
+      menus_have_icons = true;
+      show_shortcuts_in_context_menus = true;
+      stylesheets = "@Invalid()";
+      toolbutton_style = FOLLOW_APPLICATION_STYLE;
+      underline_shortcut = 1;
+      wheel_scroll_lines = 3;
+    };
+  };
+  xdg.configFile."qt5ct/qt5ct.conf".text =
+    config.xdg.configFile."qt6ct/qt6ct.conf".text;
 }
