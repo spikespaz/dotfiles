612f208bfdd0f7ed013d2ed95070a58ca4efa2f1
Jacob Birkett <jacob@birkett.dev>
2025-04-11 16:47:36 -0700

    overlays: updates: materia-kde-theme: add override for latest tag

--- overlays/updates.nix
+++ overlays/updates.nix
@@ -1,1 +1,13 @@
-pkgs: pkgs0: let inherit (pkgs) lib; in { }
+pkgs: pkgs0:
+let inherit (pkgs) lib;
+in {
+  materia-kde-theme = pkgs0.materia-kde-theme.overrideAttrs (self: _: {
+    version = "20220823";
+    src = pkgs.fetchFromGitHub {
+      owner = "PapirusDevelopmentTeam";
+      repo = "materia-kde";
+      rev = self.version;
+      hash = "sha256-/O+/L6C9WjxhfWZ8RzIeimNU+8sjKvbDvQwNlvVOjU4=";
+    };
+  });
+}
