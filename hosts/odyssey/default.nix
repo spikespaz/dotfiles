996a2bc114163c29840c32cf36c967c3bcd0d318
Jacob Birkett <jacob@birkett.dev>
2025-04-19 07:40:53 -0700

    hosts: odyssey: overlays remove hyprnix/hyprland default

--- hosts/odyssey/default.nix
+++ hosts/odyssey/default.nix
@@ -39,8 +39,6 @@ lib.bird.mkHost args {
     self.overlays.default
     # override packages with an unfree license
     self.overlays.allowUnfree
-    # window manager
-    inputs.hyprnix.overlays.default
     # override certain packages to be fetchable in binary caches
     self.overlays.hacks.nixpkgs-config-exceptions
     # other packages
