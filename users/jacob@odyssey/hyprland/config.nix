1026b91f9f016ed568f0578a682f010ae20ce829
Jacob Birkett <jacob@birkett.dev>
2025-06-15 18:19:30 -0700

    me@odsy: hl: config: configure cursor warping for keyboard control

--- users/jacob@odyssey/hyprland/config.nix
+++ users/jacob@odyssey/hyprland/config.nix
@@ -16,6 +16,13 @@
     binds = let LAST_ACTIVE_WINDOW = 1;
     in { workspace_center_on = LAST_ACTIVE_WINDOW; };
 
+    cursor = {
+      no_warps = lib.mkForce false;
+      persistent_warps = true;
+      warp_on_change_workspace = lib.mkForce true;
+      # warp_back_after_non_mouse_input = lib.mkForce true;
+    };
+
     gestures.workspace_swipe = { min_speed_to_force = lib.mkForce 10; };
   };
 }
