77e0d137c97662c3f2a60e5a8654cdfd685a83ec
Jacob Birkett <jacob@birkett.dev>
2025-06-15 23:11:54 -0700

    hm-modules: vesktop: remove in favor of recolor package

--- users/jacob/programs/vesktop.nix
+++ users/jacob/programs/vesktop.nix
@@ -1,34 +1,41 @@
-{ self, lib, ... }:
+{ lib, pkgs, ... }:
 let
   inherit (lib.birdos.colors) grayRGB listRGB formats;
   gray = p: listRGB (grayRGB p);
 
-  gruvbox-darker = with formats.listRGB.gruvbox.colors; {
-    accentcolor = neutral_orange;
-    accentcolor2 = neutral_purple;
-    linkcolor = neutral_blue;
-    mentioncolor = neutral_aqua;
-    successcolor = bright_green;
-    warningcolor = bright_yellow;
-    dangercolor = bright_red;
+  recolorThemes = mkRecolorThemes {
+    gruvbox-darker = with formats.listRGB.gruvbox.colors; {
+      accentcolor = neutral_orange;
+      accentcolor2 = neutral_purple;
+      linkcolor = neutral_blue;
+      mentioncolor = neutral_aqua;
+      successcolor = bright_green;
+      warningcolor = bright_yellow;
+      dangercolor = bright_red;
 
-    textbrightest = light0_hard;
-    textbrighter = light0;
-    textbright = light0;
-    textdark = dark4;
-    textdarker = dark3;
-    textdarkest = dark2;
+      textbrightest = light0_hard;
+      textbrighter = light0;
+      textbright = light0;
+      textdark = dark4;
+      textdarker = dark3;
+      textdarkest = dark2;
 
-    backgroundaccent = gray 0.12;
-    backgroundprimary = gray 8.0e-2;
-    backgroundsecondary = gray 6.0e-2;
-    backgroundsecondaryalt = gray 5.0e-2;
-    backgroundtertiary = gray 4.0e-2;
-    backgroundfloating = gray 0;
+      backgroundaccent = gray 0.12;
+      backgroundprimary = gray 8.0e-2;
+      backgroundsecondary = gray 6.0e-2;
+      backgroundsecondaryalt = gray 5.0e-2;
+      backgroundtertiary = gray 4.0e-2;
+      backgroundfloating = gray 0;
+
+      font = [ "system-ui" ];
+      settingsicons = false;
+    };
   };
+  mkRecolorThemes = lib.mapAttrs (themeName: overrideVariables:
+    pkgs.discord-recolor-theme.override {
+      inherit themeName overrideVariables;
+    });
 in {
-  imports = [ self.homeManagerModules.vesktop ];
-
   programs.vesktop = {
     enable = true;
     # settings = {
@@ -38,14 +45,6 @@ in {
     #   channel = "ptb";
     #   mods = "vencord";
     # };
-
-    recolorTheme = {
-      enable = true;
-      colors = gruvbox-darker;
-      extras = {
-        font = [ "system-ui" ];
-        settingsicons = false;
-      };
-    };
+    vencord.themes = recolorThemes;
   };
 }
