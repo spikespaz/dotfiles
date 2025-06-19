d32be82726fcf638c17582db89b9eee39927c178
Jacob Birkett <jacob@birkett.dev>
2025-06-18 23:43:05 -0700

    me: vscode: langs: typst: add mathematic.vscode-pdf extension

--- users/jacob/programs/vscode/languages/typst.nix
+++ users/jacob/programs/vscode/languages/typst.nix
@@ -2,9 +2,9 @@ profileName:
 { lib, pkgs, ... }: {
   programs.vscode.profiles.${profileName} = {
     extensions = let extensions = pkgs.callPackage ../marketplace.nix { };
-    in with extensions.preferReleases;
-    [ # #
+    in with extensions.preferReleases; [
       myriad-dreamin.tinymist
+      mathematic.vscode-pdf
     ];
 
     userSettings = { };
