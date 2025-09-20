79b9f87658a8618179cc63d021550cb83d602d83
Jacob Birkett <jacob@birkett.dev>
2025-09-20 05:06:36 -0700

    me: wayland: sessionVariables: convert bools to string

--- users/jacob/wayland/default.nix
+++ users/jacob/wayland/default.nix
@@ -16,20 +16,21 @@
   ];
 
   # make some environment tweaks for wayland
-  home.sessionVariables = {
+  home.sessionVariables = let b = b: if b then "1" else "0";
+  in {
     GDK_BACKEND = "wayland,x11";
     # some nixpkgs modules have wrapers
     # that force electron apps to use wayland
-    NIXOS_OZONE_WL = "1";
+    NIXOS_OZONE_WL = b true;
     # make qt apps expect wayland
     QT_QPA_PLATFORM = "wayland";
     # set backend for sdl
     SDL_VIDEODRIVER = "wayland";
     # fix modals from being attached on tiling wms
-    _JAVA_AWT_WM_NONREPARENTING = "1";
+    _JAVA_AWT_WM_NONREPARENTING = b true;
     # fix java gui antialiasing
     _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
     # firefox and mozilla software expect wayland
-    MOZ_ENABLE_WAYLAND = "1";
+    MOZ_ENABLE_WAYLAND = b true;
   };
 }
