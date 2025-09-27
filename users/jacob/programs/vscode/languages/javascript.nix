38e975762154408f908c93e5c265fb896a1f1a1c
Jacob Birkett <jacob@birkett.dev>
2025-09-26 17:49:47 -0700

    me: vscode: langs: javascript: enable inlay hints

--- users/jacob/programs/vscode/languages/javascript.nix
+++ users/jacob/programs/vscode/languages/javascript.nix
@@ -10,6 +10,13 @@ profileName:
     userSettings = {
       "prettier.prettierPath" = pkgs.prettier;
 
+      "javascript.inlayHints.functionLikeReturnTypes.enabled" = true;
+      "javascript.inlayHints.parameterNames.enabled" = true;
+      "javascript.inlayHints.parameterTypes.enabled" = true;
+      "javascript.inlayHints.propertyDeclarationTypes.enabled" = true;
+      "javascript.inlayHints.variableTypes.enabled" = true;
+      "javascript.inlayHints.variableTypes.suppressWhenTypeMatchesName" = false;
+
       "[javascript]" = {
         "editor.defaultFormatter" = "esbenp.prettier-vscode";
       };
